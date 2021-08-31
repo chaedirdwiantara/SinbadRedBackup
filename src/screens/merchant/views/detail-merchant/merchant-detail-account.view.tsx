5cb77be13d05624fe1662ea6a45d7da32c0aa914
tyo-sinbad <prasetyo@sinbad.co.id>
2021-08-31 17:03:53 +0700

    merchant -> add informasi toko view

--- src/screens/merchant/views/detail-merchant/merchant-detail-account.view.tsx
+++ src/screens/merchant/views/detail-merchant/merchant-detail-account.view.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 import { SnbContainer, SnbTopNav, SnbText } from 'react-native-sinbad-ui';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, TouchableOpacity } from 'react-native';
 import { NavigationAction } from '@navigation';
+import { color } from 'react-native-sinbad-ui';
+/** === IMPORT STYLE HERE === */
+import MerchantStyles from '../../styles/merchant.style';
 
 const MerchantDetailAccountView: FC = () => {
   /** === VIEW === */
@@ -15,11 +18,56 @@ const MerchantDetailAccountView: FC = () => {
       />
     );
   };
+  /** === RENDER CONTENT SECTION === */
+  const renderContentSection = (data: any) => {
+    return (
+      <View style={MerchantStyles.boxContent}>
+        <View>
+          <View style={{ marginBottom: 6 }}>
+            <SnbText.B3 color={color.black60}>{data.key}</SnbText.B3>
+          </View>
+          <SnbText.B3 color={color.black100}>{data.value}</SnbText.B3>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          {data.action === 'tambah' && (
+            <TouchableOpacity onPress={() => console.log('press')}>
+              <SnbText.C1 color={color.red50}>Tambah</SnbText.C1>
+            </TouchableOpacity>
+          )}
+          {data.action === 'ubah' && (
+            <TouchableOpacity onPress={() => console.log('press')}>
+              <SnbText.C1 color={color.red50}>Ubah</SnbText.C1>
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+    );
+  };
   /** => content */
   const content = () => {
     return (
-      <ScrollView scrollEventThrottle={16} showsVerticalScrollIndicator={false}>
-        <SnbText.B1>test</SnbText.B1>
+      <ScrollView contentContainerStyle={{ paddingBottom: 16 }}>
+        <View>
+          {renderContentSection({
+            key: 'Sinbad ID',
+            value: 'SNB0001',
+          })}
+          {renderContentSection({
+            key: 'Nama Toko',
+            value: '-',
+            action: 'tambah',
+          })}
+          {renderContentSection({
+            key: 'Nomor Handphone',
+            value: '-',
+            action: 'tambah',
+          })}
+          {renderContentSection({
+            key: 'Foto Toko',
+            value: '-',
+            action: 'tambah',
+          })}
+        </View>
       </ScrollView>
     );
   };
