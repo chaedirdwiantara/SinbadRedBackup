3b162a166f2035efa57c82ec73f4e8df51d1f410
ryananjasmara <ryananjasmara1997@gmail.com>
2021-08-31 13:43:28 +0700

    voucher -> add function

---
+++ src/screens/voucher/functions/index.tsx
@@ -0,0 +1,14 @@
+/** === EXPORT ALL === */
+export { VoucherFunc } from './voucher.function';
+/**
+ * ================================================================
+ * NOTES
+ * ================================================================
+ * createdBy: ryan (team)
+ * createDate: 31082021
+ * updatedBy: -
+ * updatedDate: -
+ * updatedFunction/Component:
+ * -> NaN (no desc)
+ * -> NaN (no desc)
+ */
---
+++ src/screens/voucher/functions/voucher.function.tsx
@@ -0,0 +1,9 @@
+/** === IMPORT PACKAGE HERE === */
+import { NavigationAction } from '@navigation';
+/** === FUNCTION === */
+/** => go back */
+const goBack = () => {
+  NavigationAction.back();
+};
+
+export const VoucherFunc = { goBack };
