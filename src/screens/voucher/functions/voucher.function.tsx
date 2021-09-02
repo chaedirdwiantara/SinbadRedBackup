5ead7038336ec7d3bfe57d3a5cf54a688fdc91d0
ryananjasmara <ryananjasmara1997@gmail.com>
2021-09-02 13:19:43 +0700

    voucher -> update voucher function

--- src/screens/voucher/functions/voucher.function.tsx
+++ src/screens/voucher/functions/voucher.function.tsx
@@ -1,9 +1,46 @@
 /** === IMPORT PACKAGE HERE === */
 import { NavigationAction } from '@navigation';
 /** === FUNCTION === */
-/** => go back */
+/** => GO BACK */
 const goBack = () => {
   NavigationAction.back();
+};
+/** => SEARCH VOUCHER */
+const searchVoucher = (
+  keyword: string,
+  supplierVoucherList: [],
+  sinbadVoucherList: [],
+) => {
+  const filteredSupplierVoucher = supplierVoucherList.filter((item) => {
+    return item.voucherHeader.toLowerCase().includes(keyword.toLowerCase());
+  });
+
+  const filteredSinbadVoucher = sinbadVoucherList.filter((item) => {
+    return item.voucherHeader.toLowerCase().includes(keyword.toLowerCase());
+  });
+
+  groupingVoucher({
+    sinbadVouchers: filteredSinbadVoucher,
+    supplierVouchers: filteredSupplierVoucher,
+  });
+};
+/** => GROUPING VOUCHER */
+const groupingVoucher = (voucherData: {
+  sinbadVouchers: [];
+  supplierVouchers: [];
+}) => {
+  // group supplier voucher base on faktur
+  const supplierVoucherData = voucherData.supplierVouchers.reduce(
+    (result, currentValue) => {
+      (result[currentValue.invoiceGroupId] =
+        result[currentValue.invoiceGroupId] || []).push(currentValue);
+      return result;
+    },
+    [],
+  );
+  const sinbadVoucherData = [...voucherData.sinbadVouchers];
+  // return the data
+  return [sinbadVoucherData, supplierVoucherData];
 };
 
 export const VoucherFunc = { goBack };
