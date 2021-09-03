ea4acc7b134d2afc9da870857f9500fef032fc39
mazdha94 <muhammad.ali@sinbad.co.id>
2021-09-03 18:24:11 +0700

    Add SharedNav for Shared Screen

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -2,12 +2,14 @@ import OmsNav from './oms.navigation';
 import ExampleNav from './example.navigation';
 import Example2Nav from './example2.navigation';
 import AuthNav from './auth.navigation';
+import SharedNav from './shared.navigation';
 
 const index = {
   ...OmsNav,
   ...ExampleNav,
   ...Example2Nav,
   ...AuthNav,
+  ...SharedNav,
 };
 
 export default index;
