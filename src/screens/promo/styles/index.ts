0a3470a78544829310f17dac0ab874e84548a3f8
ryananjasmara <ryananjasmara1997@gmail.com>
2021-09-27 17:23:12 +0700

    promo -> add promo payment list styles

---
+++ src/screens/promo/styles/index.ts
@@ -0,0 +1,1 @@
+export * from './promo-payment-list.style';
---
+++ src/screens/promo/styles/promo-payment-list.style.ts
@@ -0,0 +1,4 @@
+import { StyleSheet } from 'react-native';
+import { color } from 'react-native-sinbad-ui';
+
+export const PromoPaymentListStyles = StyleSheet.create({});
