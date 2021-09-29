fd476f824b427a6c8f1b23aec918f6d4afeca76d
ryananjasmara <ryananjasmara1997@gmail.com>
2021-09-29 10:07:22 +0700

    promo -> finish layout on promo payment list

--- src/screens/promo/views/promo-payment-list.view.tsx
+++ src/screens/promo/views/promo-payment-list.view.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
-import { SnbContainer, SnbTopNav, SnbProgress } from 'react-native-sinbad-ui';
+import { View, Image, ScrollView } from 'react-native';
+import {
+  SnbContainer,
+  SnbTopNav,
+  SnbProgress,
+  SnbCardButtonType1,
+} from 'react-native-sinbad-ui';
+import moment from 'moment';
 import { goBack } from '../functions';
 import { PromoPaymentListStyles } from '../styles';
 import { contexts } from '@contexts';
@@ -9,6 +15,20 @@ import { toCurrency } from '@core/functions/global/currency-format';
 import * as Actions from '@actions';
 import { useDispatch } from 'react-redux';
 import { useDataGlobal } from '@core/redux/Data';
+/** === DUMMIES === */
+const dummies = {
+  data: [
+    {
+      paymentPromoId: 1,
+      paymentPromoName: 'Promo Virtual Account BCA',
+      paymentPromoDescription: 'Promo potongan sebesar Rp50.000',
+      imageUrl:
+        'https://artolouis.com/wp-content/uploads/2019/05/Bank_Central_Asia.png',
+      startDate: '2021-07-01T16:59:00.000Z',
+      expiredAt: '2021-07-31T16:59:00.000Z',
+    },
+  ],
+};
 /** === COMPONENT === */
 const PromoPaymentList: FC = () => {
   /** === HOOK === */
@@ -24,6 +44,55 @@ const PromoPaymentList: FC = () => {
       />
     );
   };
+  /** => image payment promo */
+  const renderImagePaymentPromo = (imageUrl: string) => {
+    return (
+      <View
+        style={{
+          alignContent: 'flex-start',
+          marginRight: 16,
+          alignSelf: 'center',
+        }}>
+        <Image
+          source={{ uri: imageUrl }}
+          style={{
+            width: 60,
+            height: 40,
+            borderRadius: 20,
+          }}
+        />
+      </View>
+    );
+  };
+  /** => promo list */
+  const renderPromoList = () => {
+    return (
+      <View style={{ marginHorizontal: 16, marginTop: 16 }}>
+        {dummies.data.map((item, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                paddingVertical: 8,
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}>
+              <SnbCardButtonType1
+                title={item.paymentPromoName}
+                subTitle1={item.paymentPromoDescription}
+                subTitle2={`Berlaku ${moment(new Date(item.startDate)).format(
+                  'DD MMM',
+                )} - ${moment(new Date(item.expiredAt)).format('DD MMM YYYY')}`}
+                left={() => renderImagePaymentPromo(item.imageUrl)}
+                type={'goTo'}
+                onPress={() => goBack()}
+              />
+            </View>
+          );
+        })}
+      </View>
+    );
+  };
   /** => loading */
   const renderLoading = () => {
     return (
@@ -36,7 +105,7 @@ const PromoPaymentList: FC = () => {
   return (
     <SnbContainer color="grey">
       {renderHeader()}
-      {renderLoading()}
+      <ScrollView>{renderPromoList()}</ScrollView>
     </SnbContainer>
   );
 };
