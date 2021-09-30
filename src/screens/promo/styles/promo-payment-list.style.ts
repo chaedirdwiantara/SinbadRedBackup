0908940f062fa76fab8f507d7dcf2336f4eed6fa
ryananjasmara <ryananjasmara1997@gmail.com>
2021-09-30 12:05:23 +0700

    promo -> update promo payment list styles

--- src/screens/promo/styles/promo-payment-list.style.ts
+++ src/screens/promo/styles/promo-payment-list.style.ts
@@ -1,4 +1,25 @@
 import { StyleSheet } from 'react-native';
-import { color } from 'react-native-sinbad-ui';
 
-export const PromoPaymentListStyles = StyleSheet.create({});
+export const PromoPaymentListStyles = StyleSheet.create({
+  singleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    paddingVertical: 8,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  image: {
+    width: 60,
+    height: 40,
+    borderRadius: 20,
+    resizeMode: 'contain',
+  },
+  imageContainer: {
+    alignContent: 'flex-start',
+    marginRight: 16,
+    alignSelf: 'center',
+  },
+});
