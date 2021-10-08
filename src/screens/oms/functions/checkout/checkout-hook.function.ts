07b0a868295c355eb910b51839d6c9e9ea100adb
ryananjasmara <ryananjasmara1997@gmail.com>
2021-10-08 16:43:59 +0700

    oms checkout -> update functions and styles

---
+++ src/screens/oms/functions/checkout/checkout-hook.function.ts
@@ -0,0 +1,44 @@
+/** === IMPORT PACKAGE HERE === */
+import React from 'react';
+import { useDispatch } from 'react-redux';
+/** === IMPORT EXTERNAL FUNCTION HERE === */
+import * as Actions from '@actions';
+/** === FUNCTION === */
+/** => promo general action */
+const usePaymentDetailAccorrdion = () => {
+  const [active, setActive] = React.useState<number | null>(null);
+  return {
+    changeActive: (index: number) => {
+      if (index === active) {
+        setActive(null);
+      } else {
+        setActive(index);
+      }
+    },
+    active,
+  };
+};
+/** => payment types modal */
+const usePaymentTypeModal = () => {
+  const [isOpen, setOpen] = React.useState(false);
+  return {
+    setOpen: (value: boolean) => {
+      setOpen(value);
+    },
+    isOpen,
+  };
+};
+/** === EXPORT === */
+export { usePaymentDetailAccorrdion, usePaymentTypeModal };
+/**
+ * ================================================================
+ * NOTES
+ * ================================================================
+ * createdBy: ryan (team)
+ * createDate: 07102021
+ * updatedBy: -
+ * updatedDate: -
+ * updatedFunction/Component:
+ * -> NaN (no desc)
+ * -> NaN (no desc)
+ */
---
+++ src/screens/oms/functions/checkout/index.ts
@@ -0,0 +1,14 @@
+/** === EXPORT ALL === */
+export * from './checkout-hook.function';
+/**
+ * ================================================================
+ * NOTES
+ * ================================================================
+ * createdBy: ryan (team)
+ * createDate: 07102021
+ * updatedBy: -
+ * updatedDate: -
+ * updatedFunction/Component:
+ * -> NaN (no desc)
+ * -> NaN (no desc)
+ */
