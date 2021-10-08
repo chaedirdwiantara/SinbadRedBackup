42133852ada3f672a1975686656982f39fd8a6a0
aliisetia <ali.setia@sinbad.co.id>
2021-10-08 14:02:07 +0700

    Style Category Home View

---
+++ src/screens/category/styles/category-home.style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const CategoryHomeStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  itemContainer: {
+    height: 54,
+    width: 54,
+    marginHorizontal: 14,
+    borderRadius: 16,
+  },
+  itemName: {
+    justifyContent: 'center',
+    marginTop: 8,
+    paddingHorizontal: 10,
+  },
+});
+
+export default CategoryHomeStyle;
