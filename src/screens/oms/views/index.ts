008f43e36bdfe3152b48a355af4d130e844c17a3
ryananjasmara <ryananjasmara1997@gmail.com>
2021-10-14 11:00:52 +0700

    oms -> remove unused views

--- src/screens/oms/views/index.ts
+++ src/screens/oms/views/index.ts
@@ -1,8 +1,5 @@
 /** === IMPORT ALL MODULE'S SCREEN VIEW HERE === */
 import OmsShoppingCartView from './shopping-cart/shopping-cart.view';
-import OmsShoppingCart2View from './oms-shopping-cart2.view';
-import OmsShoppingCart3View from './oms-shopping-cart3.view';
-import OmsShoppingCart4View from './oms-shopping-cart4.view';
 import OmsVerificationOrderView from './verification-order/verification-order.view';
 import OmsCheckoutView from './checkout/checkout.view';
 import OmsCheckoutSuccessView from './checkout-success/checkout-success.view';
