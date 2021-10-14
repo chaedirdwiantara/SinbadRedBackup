4bfaad62ffd67c84e015c09365f9bfe68c5cf092
bagaspp-sinbad <bagas.prasetya@sinbad.co.id>
2021-10-14 14:05:17 +0700

    resolve conflict

--- src/data/types/index.ts
+++ src/data/types/index.ts
@@ -14,11 +14,8 @@ export * from './oms/verification-order/verification-order.type';
 export * from './promo.type';
 export * from './category.type';
 export * from './voucher.type';
-<<<<<<< HEAD
 export * from './brand.type';
-=======
 export * from './user.type';
->>>>>>> 1d0fabf6c7781738c513d0e502e16bb3874fa1c1
 /**
  * ================================================================
  * NOTES
