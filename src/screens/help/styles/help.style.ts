1f42d4145a67bd83cafda6cf0f8b5f4a104a5ec1
bagaspp-sinbad <bagas.prasetya@sinbad.co.id>
2021-10-18 13:56:03 +0700

    add help screen

---
+++ src/screens/help/styles/help.style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { styles } from 'react-native-sinbad-ui';
+
+const HelpStyles = StyleSheet.create({
+  headerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 16,
+    textAlign: 'center',
+  },
+  image145Contain: {
+    height: 145,
+    width: undefined,
+    aspectRatio: 1 / 1,
+    resizeMode: 'contain',
+  },
+});
+
+export default HelpStyles;
--- src/screens/help/views/help.view.tsx
+++ src/screens/help/views/help.view.tsx
@@ -1,12 +1,114 @@
-import React, { FC } from 'react';
-import { View } from 'react-native';
-import { SnbContainer, SnbTopNav } from 'react-native-sinbad-ui';
+import React, { FC, useState } from 'react';
+import { View, Image, ScrollView } from 'react-native';
+import {
+  SnbContainer,
+  SnbTopNav,
+  SnbText,
+  SnbButton,
+  color,
+} from 'react-native-sinbad-ui';
+import DeviceInfo from 'react-native-device-info';
+/** === IMPORT STYLE HERE === */
+import HelpStyle from '../styles/help.style';
 
 const HelpView: FC = () => {
-  /** this for main view */
+  const [menu] = useState([
+    {
+      name: 'Hubungi Customer Services',
+      goTo: 'call_cs',
+    },
+    {
+      name: 'FAQ',
+      goTo: 'faq',
+    },
+    {
+      name: 'Clear Cache',
+      goTo: 'clear_cache',
+    },
+  ]);
+  /** === HOOK === */
+  /** === VIEW === */
+  /** === HEADER === */
+  const header = () => {
+    return <SnbTopNav.Type1 type="red" title="Bantuan" />;
+  };
+  /** === RENDER CONTENT HEADER ===*/
+  const renderContentHeader = () => {
+    return (
+      <View style={HelpStyle.headerContainer}>
+        <Image
+          style={[HelpStyle.image145Contain, { marginBottom: 10 }]}
+          source={require('../../../assets/images/sinbad_image/help_sinbad.png')}
+        />
+        <SnbText.B4 align="center">Sobat Sinbad butuh bantuan ?</SnbText.B4>
+        <SnbText.B4 align="center">
+          Silahkan bertanya, akan kami bantu menyelesaikan pertanyaan sobat !
+        </SnbText.B4>
+      </View>
+    );
+  };
+  /** === RENDER MENU CONTENT === */
+  const renderContentMenu = () => {
+    return menu.map((item, index) => {
+      return (
+        <>
+          <View key={index} style={{ paddingHorizontal: 16 }}>
+            <SnbButton.Dynamic
+              title={item.name}
+              type="tertiary"
+              size="small"
+              position="left"
+              disabled={false}
+              contentColor={color.black100}
+              loading={false}
+              onPress={() => console.log(`pressed ${item.name}`)}
+            />
+          </View>
+          <View
+            style={{
+              borderTopWidth: 1,
+              borderColor: color.black10,
+              marginLeft: 16,
+            }}
+          />
+        </>
+      );
+    });
+  };
+  /** === RENDER VERSION CONTENT === */
+  const renderContentVersion = () => {
+    const envName = (w = DeviceInfo.getApplicationName()) => {
+      let words = w.split(' ');
+      if (words.length === 2) {
+        return words[1]; // return Development, Staging, ""
+      } else {
+        return '';
+      }
+    };
+
+    const textVersion = `${envName()} Versi ${DeviceInfo.getVersion()} ${DeviceInfo.getBuildNumber()}`;
+
+    return (
+      <View style={{ padding: 16 }}>
+        <SnbText.B3 color={color.black100}>{textVersion}</SnbText.B3>
+      </View>
+    );
+  };
+  /** === RENDER CONTENT === */
+  const renderContent = () => {
+    return (
+      <ScrollView>
+        {renderContentHeader()}
+        {renderContentMenu()}
+        {renderContentVersion()}
+      </ScrollView>
+    );
+  };
+  /** === MAIN === */
   return (
-    <SnbContainer>
-      <SnbTopNav.Type1 type="red" title="Bantuan" />
+    <SnbContainer color="white">
+      {header()}
+      {renderContent()}
     </SnbContainer>
   );
 };
