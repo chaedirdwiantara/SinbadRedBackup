4c1f6f9962d7241f105068fbf918b9891c6b212f
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-10-22 08:47:36 +0700

    convert map render to flatlist banner list

--- src/screens/banner/views/banner.view.tsx
+++ src/screens/banner/views/banner.view.tsx
@@ -1,6 +1,6 @@
 /** === IMPORT PACKAGE HERE ===  */
-import React, { FC } from 'react';
-import { TouchableOpacity, View, Image, ScrollView } from 'react-native';
+import React from 'react';
+import { TouchableOpacity, View, Image, FlatList } from 'react-native';
 import {
   SnbContainer,
   SnbTopNav,
@@ -11,6 +11,38 @@ import {
 } from 'react-native-sinbad-ui';
 import { goBack, goToBannerDetail } from '../functions';
 import { BannerStyles } from '../styles';
+
+/** === MOCK === */
+const bannerList = [
+  {
+    id: 1,
+    imageUrl:
+      'https://images.tokopedia.net/img/cache/1200/NXCtjv/2021/9/22/9f12eb8f-41d9-4618-83eb-f47cd636617f.png.webp',
+    title: 'Khusus untuk kamu, iya kamu ! Dapatkan Promo Voucher SGM',
+    description:
+      'SINBAD mengadakan diskon hingga 5% untuk pembelian SGM. Jangan sampai ketinggalan promo dari SINBAD !',
+    expiredAt: '31 Jan 2020',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://images.tokopedia.net/img/cache/1200/NXCtjv/2021/9/22/9f12eb8f-41d9-4618-83eb-f47cd636617f.png.webp',
+    title: 'Khusus untuk kamu, iya kamu ! Dapatkan Promo Voucher SGM',
+    description:
+      'SINBAD mengadakan diskon hingga 5% untuk pembelian SGM. Jangan sampai ketinggalan promo dari SINBAD !',
+    expiredAt: '31 Jan 2020',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://images.tokopedia.net/img/cache/1200/NXCtjv/2021/9/22/9f12eb8f-41d9-4618-83eb-f47cd636617f.png.webp',
+    title: 'Khusus untuk kamu, iya kamu ! Dapatkan Promo Voucher SGM',
+    description:
+      'SINBAD mengadakan diskon hingga 5% untuk pembelian SGM. Jangan sampai ketinggalan promo dari SINBAD !',
+    expiredAt: '31 Jan 2020',
+  },
+];
+
 /** === COMPONENT === */
 const BannerListView: React.FC = () => {
   /** === HOOK === */
@@ -47,22 +79,68 @@ const BannerListView: React.FC = () => {
     );
   };
 
+  /** => list banner */
+  const renderListBanner = ({ item }: { item: any; index: number }) => {
+    return (
+      <View style={BannerStyles.bannerCardContainer}>
+        {/* Image */}
+        <View>
+          <Image
+            style={BannerStyles.imageCard}
+            source={{
+              uri: item.imageUrl,
+            }}
+          />
+        </View>
+        {/* Info */}
+        <View style={{ padding: 16, backgroundColor: 'white' }}>
+          <SnbText.B2>{item.title}</SnbText.B2>
+          <View style={{ marginTop: 8 }}>
+            <SnbText.B3 color={color.black80}>{item.description}</SnbText.B3>
+          </View>
+        </View>
+        {/* Foter */}
+        <View style={BannerStyles.footerCardBanner}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <SnbIcon name={'calender'} color={color.black60} size={16} />
+            <View style={{ marginLeft: 7 }}>
+              <SnbText.C1 color={color.black60}>
+                Berlaku sampai {item.expiredAt}
+              </SnbText.C1>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={BannerStyles.buttonDetail}
+            onPress={() => goToBannerDetail()}>
+            <SnbText.B2 color={'white'}>Detail</SnbText.B2>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   /** => content */
   const content = () => {
     return (
       <View style={{ margin: 16 }}>
-        {[0, 1, 2, 3].map((item, index) => (
-          <CardBanner key={index.toString()} />
-        ))}
+        <FlatList
+          data={bannerList}
+          renderItem={renderListBanner}
+          keyExtractor={(item) => item.id.toString()}
+          onEndReachedThreshold={0.1}
+          onEndReached={() => console.log('List scroll has reached end')}
+          showsVerticalScrollIndicator={true}
+        />
       </View>
     );
   };
+
   /** => main */
   return (
     <SnbContainer color="white">
       {header()}
       {search()}
-      <ScrollView>{content()}</ScrollView>
+      {content()}
     </SnbContainer>
   );
 };
@@ -80,47 +158,3 @@ export default BannerListView;
  * -> NaN (no desc)
  * -> NaN (no desc)
  */
-
-const CardBanner = () => {
-  return (
-    <View style={BannerStyles.bannerCardContainer}>
-      {/* Image */}
-      <View>
-        <Image
-          style={BannerStyles.imageCard}
-          source={{
-            uri: 'https://images.tokopedia.net/img/cache/1200/NXCtjv/2021/9/22/9f12eb8f-41d9-4618-83eb-f47cd636617f.png.webp',
-          }}
-        />
-      </View>
-      {/* Info */}
-      <View style={{ padding: 16, backgroundColor: 'white' }}>
-        <SnbText.B2>
-          Khusus untuk kamu, iya kamu ! Dapatkan Promo Voucher SGM
-        </SnbText.B2>
-        <View style={{ marginTop: 8 }}>
-          <SnbText.B3 color={color.black80}>
-            SINBAD mengadakan diskon hingga 5% untuk pembelian SGM. Jangan
-            sampai ketinggalan promo dari SINBAD !
-          </SnbText.B3>
-        </View>
-      </View>
-      {/* Foter */}
-      <View style={BannerStyles.footerCardBanner}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <SnbIcon name={'calender'} color={color.black60} size={16} />
-          <View style={{ marginLeft: 7 }}>
-            <SnbText.C1 color={color.black60}>
-              Berlaku sampai 31 Jan 2020
-            </SnbText.C1>
-          </View>
-        </View>
-        <TouchableOpacity
-          style={BannerStyles.buttonDetail}
-          onPress={() => goToBannerDetail()}>
-          <SnbText.B2 color={'white'}>Detail</SnbText.B2>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
