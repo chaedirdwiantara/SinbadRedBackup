f407a784e438872813337ca4bc3cfe7f7b089235
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-10-26 01:57:12 +0700

    create function saga product detail

--- src/data/sagas/product.saga.ts
+++ src/data/sagas/product.saga.ts
@@ -16,14 +16,29 @@ function* productList(action: models.ListProcessAction) {
     );
     yield action.contextDispatch(ActionCreators.productListSuccess(response));
     yield put(ActionCreators.productListSuccess(response));
-  } catch (error) {
+  } catch (error: any) {
     yield action.contextDispatch(ActionCreators.productListFailed(error));
     yield put(ActionCreators.productListFailed(error));
+  }
+}
+/** => product detail */
+function* productDetail(action: models.DetailProcessAction) {
+  try {
+    const response: models.DetailSuccessProps<models.ProductDetailSuccessProps> =
+      yield call(() => {
+        return ProductApi.productDetail(action.payload);
+      });
+    yield action.contextDispatch(ActionCreators.productDetailSuccess(response));
+    yield put(ActionCreators.productDetailSuccess(response));
+  } catch (error: any) {
+    yield action.contextDispatch(ActionCreators.productDetailFailed(error));
+    yield put(ActionCreators.productDetailFailed(error));
   }
 }
 /** === LISTEN FUNCTION === */
 function* ProductSaga() {
   yield takeLatest(types.PRODUCT_LIST_PROCESS, productList);
+  yield takeLatest(types.PRODUCT_DETAIL_PROCESS, productDetail);
 }
 
 export default ProductSaga;
