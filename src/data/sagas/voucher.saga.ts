36df351b93762a9ec90a2c4c8066f65cad7eafc6
ryananjasmara <ryananjasmara1997@gmail.com>
2021-10-27 14:59:06 +0700

    voucher -> add count voucher sagas

--- src/data/sagas/voucher.saga.ts
+++ src/data/sagas/voucher.saga.ts
@@ -7,7 +7,7 @@ import * as types from '@types';
 import * as models from '@models';
 /** === FUNCTION === */
 /** => voucher detail */
-function* voucherDetail(action: models.DetailProcessAction) {
+function* voucherDetail(action: models.VoucherDetailProcessAction) {
   try {
     const response: models.DetailSuccessProps<models.VoucherDetailProps> =
       yield call(() => {
@@ -36,10 +36,27 @@ function* voucherCartList(action: models.DetailProcessAction) {
     yield put(ActionCreators.voucherCartListFailed(error));
   }
 }
+/** => count all voucher */
+function* countAllVoucher(action: models.DetailProcessAction) {
+  try {
+    const response: models.DetailSuccessProps<models.CountAllVoucherProps> =
+      yield call(() => {
+        return VoucherApi.countAllVoucher();
+      });
+    yield action.contextDispatch(
+      ActionCreators.countAllVoucherSuccess(response),
+    );
+    yield put(ActionCreators.countAllVoucherSuccess(response));
+  } catch (error: any) {
+    // yield action.contextDispatch(ActionCreators.voucherCartListFailed(error));
+    yield put(ActionCreators.countAllVoucherFailed(error));
+  }
+}
 /** === LISTEN FUNCTION === */
 function* VoucherSaga() {
   yield takeLatest(types.VOUCHER_DETAIL_PROCESS, voucherDetail);
   yield takeLatest(types.VOUCHER_CART_LIST_PROCESS, voucherCartList);
+  yield takeLatest(types.COUNT_ALL_VOUCHER_PROCESS, countAllVoucher);
 }
 
 export default VoucherSaga;
