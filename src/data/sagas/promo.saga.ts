fda492124230252053c3d01445980f049c1c47dc
ryananjasmara <ryananjasmara1997@gmail.com>
2021-10-29 14:49:18 +0700

    fix missing things in state managements

--- src/data/sagas/promo.saga.ts
+++ src/data/sagas/promo.saga.ts
@@ -34,7 +34,9 @@ function* promoPaymentDetail(action: models.DetailProcessAction) {
     );
     yield put(ActionCreators.promoPaymentDetailSuccess(response));
   } catch (error: any) {
-    // yield action.contextDispatch(ActionCreators.promoPaymentDetailFailed(error));
+    yield action.contextDispatch(
+      ActionCreators.promoPaymentDetailFailed(error),
+    );
     yield put(ActionCreators.promoPaymentDetailFailed(error));
   }
 }
@@ -50,7 +52,9 @@ function* promoGeneralDetail(action: models.DetailProcessAction) {
     );
     yield put(ActionCreators.promoGeneralDetailSuccess(response));
   } catch (error: any) {
-    // yield action.contextDispatch(ActionCreators.promoPaymentDetailFailed(error));
+    yield action.contextDispatch(
+      ActionCreators.promoGeneralDetailFailed(error),
+    );
     yield put(ActionCreators.promoGeneralDetailFailed(error));
   }
 }
