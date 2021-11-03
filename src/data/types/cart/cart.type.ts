747f17cc19828d3326c4560263e8fc30b20b3a76
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-11-03 08:43:39 +0700

    model cart success props

---
+++ src/data/types/cart/cart.type.ts
@@ -0,0 +1,6 @@
+/** => List */
+export const CART_LIST_PROCESS = 'CartListProcess';
+export const CART_LIST_SUCCESS = 'CartListSuccess';
+export const CART_LIST_FAILED = 'CartListFailed';
+export const CART_LIST_REFRESH = 'CartListRefresh';
+export const CART_LIST_RESET = 'CartListReset';
