102f13f4980c605eefbaf24b647fde5647cff59e
ryananjasmara <ryananjasmara1997@gmail.com>
2021-11-03 11:33:43 +0700

    verification order -> update integration on GET verification-order

--- src/data/apis/oms/verification-order/verification-order.api.ts
+++ src/data/apis/oms/verification-order/verification-order.api.ts
@@ -1,13 +1,12 @@
 /** === IMPORT EXTERNAL FUNCTION === */
+import apiMapping from '@core/services/apiMapping';
 import apiMappingMock from '@core/services/apiMappingMock';
 import * as models from '@models';
 /** === FUNCTION === */
 /** => verfication order create */
 const verficationOrderCreate = (data: {}) => {
-  const mockHost =
-    'https://690d9a8b-8da9-4142-b577-d543b2682e7f.mock.pstmn.io/';
   const path = 'verification-order';
-  return apiMappingMock(mockHost, path, 'discount', 'v1', 'CREATE', data);
+  return apiMapping('auth', path, 'discount', 'v1', 'CREATE', data);
 };
 /** => verfication order detail */
 const verficationOrderDetail = (data: models.DetailProcessProps) => {
--- src/screens/oms/views/shopping-cart/shopping-cart.view.tsx
+++ src/screens/oms/views/shopping-cart/shopping-cart.view.tsx
@@ -398,7 +398,67 @@ const OmsShoppingCartView: FC = () => {
       title="Konfirmasi"
       content="Konfirmasi order dan lanjut ke Checkout?"
       ok={() =>
-        verificationOrderCreate(dispatchVerificationOrder, { data: {} })
+        verificationOrderCreate(dispatchVerificationOrder, {
+          data: {
+            _id: 1,
+            data: [
+              {
+                invoiceGroupId: '1',
+                portfolioId: null,
+                brands: [
+                  {
+                    brandId: '0684fb26-00bf-11ec-9a03-0242ac130003',
+                    products: [
+                      {
+                        productId: '9536f526-2447-11ec-9621-0242ac130002',
+                        qty: 2,
+                        displayPrice: 201000,
+                        priceBeforeTax: 201000,
+                        priceAfterTax: 221100,
+                        warehouseId: 1,
+                      },
+                      {
+                        productId: '997fd26a-2447-11ec-9621-0242ac130002',
+                        qty: 1,
+                        displayPrice: 216000,
+                        priceBeforeTax: 216000,
+                        priceAfterTax: 237600,
+                        warehouseId: 1,
+                      },
+                      {
+                        productId: '19d816be-24db-11ec-9621-0242ac130002',
+                        qty: 2,
+                        displayPrice: 240000,
+                        priceBeforeTax: 240000,
+                        priceAfterTax: 264000,
+                        warehouseId: 1,
+                      },
+                      {
+                        productId: '32039060-24db-11ec-9621-0242ac130002',
+                        qty: 2,
+                        displayPrice: 204000,
+                        priceBeforeTax: 204000,
+                        priceAfterTax: 224400,
+                        warehouseId: 1,
+                      },
+                    ],
+                  },
+                ],
+                supplierId: 1,
+                channelId: 1,
+                groupId: 1,
+                typeId: 1,
+                clusterId: 1,
+              },
+            ],
+            isActiveStore: false,
+            voucherIds: [],
+            storeId: 1,
+            salesId: 1,
+            platform: 'sinbad_app',
+            userId: 1,
+          },
+        })
       }
       cancel={() => setIsConfirmCheckoutDialogOpen(false)}
       loading={stateVerificationOrder.create.loading}
