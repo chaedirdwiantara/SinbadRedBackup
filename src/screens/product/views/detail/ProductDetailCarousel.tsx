ef9edb4f04889fafd6c721ee6302e013b4fcaa02
aliisetia <ali.setia@sinbad.co.id>
2021-11-08 10:26:21 +0700

    Add Product Detail Integration

---
+++ src/screens/product/views/detail/ProductDetailCarousel.tsx
@@ -0,0 +1,55 @@
+/** === IMPORT PACKAGES ===  */
+import React, { FC, useRef, useState, useMemo } from 'react';
+import { View, Dimensions, Image } from 'react-native';
+import { SnbCarousel, SnbCarouselIndicator } from 'react-native-sinbad-ui';
+/** === IMPORT TYPE ===  */
+import * as models from '@models';
+/** === IMPORT STYLE ===  */
+import { ProductDetailStyle } from '@screen/product/styles';
+/** === TYPE ===  */
+interface ProductDetailCarouselProps {
+  images: Array<models.ProductImage>;
+}
+/** === CONSTANT ===  */
+const { width } = Dimensions.get('window');
+/** === COMPONENT ===  */
+export const ProductDetailCarousel: FC<ProductDetailCarouselProps> = ({
+  images,
+}) => {
+  /** === HOOKS === */
+  const carouselRef = useRef();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const imageUrls = useMemo(() => images.map((image) => image.url), [images]);
+  /** === VIEW === */
+  /** => Carousel Item */
+  const renderCarouselItem = ({ item }: { item: string }) => (
+    <Image
+      defaultSource={require('../../../../assets/images/sinbadopacity.png')}
+      source={{ uri: item }}
+      style={{ width: width, height: width }}
+    />
+  );
+  /** => Main */
+  return (
+    <View style={{ height: width }}>
+      <View style={{ flex: 1 }}>
+        <SnbCarousel
+          carouselRef={carouselRef}
+          data={imageUrls}
+          renderItem={renderCarouselItem}
+          itemWidth={width}
+          sliderWidth={width}
+          setCurrentActive={setActiveImageIndex}
+          loop={true}
+        />
+      </View>
+      <View style={ProductDetailStyle.carouselIndicatorContainer}>
+        <SnbCarouselIndicator
+          carouselRef={carouselRef}
+          dotsLength={images.length}
+          activeDotIndex={activeImageIndex}
+        />
+      </View>
+    </View>
+  );
+};
---
+++ src/screens/product/views/detail/PromoModal.tsx
@@ -0,0 +1,41 @@
+/** === IMPORT PACKAGES ===  */
+import React, { FC } from 'react';
+import { View, ScrollView, Dimensions } from 'react-native';
+import { SnbBottomSheet, SnbText } from 'react-native-sinbad-ui';
+/** === IMPORT TYPE ===  */
+import * as models from '@models';
+/** === TYPE ===  */
+interface PromoModalProps {
+  visible: boolean;
+  onClose: () => void;
+  promoList: Array<models.PotentialPromoProductProps>;
+}
+/** === CONSTANT === */
+const { height } = Dimensions.get('window');
+/** === COMPONENT ===  */
+export const PromoModal: FC<PromoModalProps> = ({
+  visible,
+  onClose,
+  promoList,
+}) => (
+  <SnbBottomSheet
+    open={visible}
+    title="Promosi"
+    actionIcon="close"
+    closeAction={onClose}
+    content={
+      <View style={{ paddingHorizontal: 16, paddingBottom: 16 }}>
+        <SnbText.B4>Potongan Harga</SnbText.B4>
+        <View style={{ height: height * 0.15, marginTop: 16 }}>
+          <ScrollView>
+            {promoList.map((promo, promoIndex) => (
+              <View key={promoIndex} style={{ marginBottom: 8 }}>
+                <SnbText.C1>{promo.shortDescription}</SnbText.C1>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+      </View>
+    }
+  />
+);
