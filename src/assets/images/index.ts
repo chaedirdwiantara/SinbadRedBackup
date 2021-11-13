7f62fdd135efe41b782fd10e0fbcdd51ac69b7c5
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-11-13 14:10:27 +0700

    handle export image

---
+++ src/assets/images/index.ts
@@ -0,0 +1,13 @@
+export * from './banner';
+export * from './faq';
+export * from './sinbad_image';
+
+export const Images = {
+  ktp: require('./ktp_image.png'),
+  npwp: require('./npwp_image.png'),
+  checkoutSuccess: require('./oms_checkout_success.png'),
+  emptyCart: require('./oms_empty_cart.png'),
+  selfie: require('./selfie_image.png'),
+  store: require('./store_image.png'),
+  emptyVoucher: require('./voucher_empty'),
+};
