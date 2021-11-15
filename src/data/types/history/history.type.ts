0ff04bddaa8aef2144900099923751db0bdbba31
Tatas Fachrul <tatasfachrul@gmail.com>
2021-11-15 17:09:08 +0700

    SR-510 -> Create history order status type

---
+++ src/data/types/history/history.type.ts
@@ -0,0 +1,5 @@
+/** HISTORY ORDER STATUS */
+export const HISTORY_ORDER_STATUS_PROCESS = 'HistoryOrderStatusProcess';
+export const HISTORY_ORDER_STATUS_SUCCESS = 'HistoryOrderStatusSuccess';
+export const HISTORY_ORDER_STATUS_FAILED = 'HistoryOrderStatusFailed';
+export const HISTORY_ORDER_STATUS_REFRESH = 'HistoryOrderStatusRefresh';
