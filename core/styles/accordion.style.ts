d39d1b80f8551e6fa3878cf405934342dad01d9a
aliisetia <ali.setia@sinbad.co.id>
2021-11-23 17:48:28 +0700

    Add Accordion and HorizontalGridLayout Product as core components

---
+++ core/styles/accordion.style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { color } from 'react-native-sinbad-ui';
+
+export const AccordionStyle = StyleSheet.create({
+  panel: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    borderBottomColor: color.black10,
+    borderBottomWidth: 1,
+  },
+  contentContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 10,
+    paddingBottom: 16,
+  },
+});
