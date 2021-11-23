0127a18a271fb41b943b22e95261b29c2fa2bcd8
aliisetia <ali.setia@sinbad.co.id>
2021-11-23 17:48:28 +0700

    Add Accordion and HorizontalGridLayout Product as core components

---
+++ core/components/Accordion.tsx
@@ -0,0 +1,49 @@
+/** === IMPORT PACKAGES ===  */
+import React, { FC, useState } from 'react';
+import { View, TouchableWithoutFeedback, LayoutAnimation } from 'react-native';
+import { SnbText, SnbIcon, color } from 'react-native-sinbad-ui';
+/** === IMPORT STYLE ===  */
+import { AccordionStyle } from '@core/styles';
+/** === TYPE === */
+interface AccordionProps {
+  data: Array<{ name: string; description: string }>;
+}
+/** === COMPONENT === */
+export const Accordion: FC<AccordionProps> = ({ data }) => {
+  /** == HOOK === */
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  /** == VIEW === */
+  return (
+    <View style={{ flex: 1, backgroundColor: color.white }}>
+      {data.map((item, itemIndex) => (
+        <View key={itemIndex}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              LayoutAnimation.configureNext(
+                LayoutAnimation.Presets.easeInEaseOut,
+              );
+              setActiveIndex((prevActive) => {
+                if (prevActive === itemIndex) {
+                  return null;
+                }
+                return itemIndex;
+              });
+            }}>
+            <View style={AccordionStyle.panel}>
+              <SnbText.B3>{item.name}</SnbText.B3>
+              <SnbIcon
+                name={itemIndex === activeIndex ? 'expand_less' : 'expand_more'}
+                size={30}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          {itemIndex === activeIndex && (
+            <View style={AccordionStyle.contentContainer}>
+              <SnbText.B3>{item.description}</SnbText.B3>
+            </View>
+          )}
+        </View>
+      ))}
+    </View>
+  );
+};
