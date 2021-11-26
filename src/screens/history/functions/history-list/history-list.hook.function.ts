4c959d2c272c08b1eaa3dc085026a086c7d679e1
Tatas Fachrul <tatasfachrul@gmail.com>
2021-11-26 19:41:00 +0700

    SR-510 -> Fetch api

--- src/screens/history/functions/history-list/history-list.hook.function.ts
+++ src/screens/history/functions/history-list/history-list.hook.function.ts
@@ -31,3 +31,48 @@ export const usePaymentStatus = () => {
     },
   };
 };
+
+/** => List */
+const callProcessAction = (
+  contextDispatch: (action: any) => any,
+  loading: boolean,
+  skip: number,
+  limit: number,
+) => {
+  return Actions.historyListProcess(contextDispatch, {
+    loading,
+    skip,
+    limit,
+  });
+};
+export const useHistoryListActions = () => {
+  const dispatch = useDispatch();
+  const limit = 10;
+
+  return {
+    fetch: (contextDispatch: (action: any) => any) => {
+      contextDispatch(Actions.historyListReset());
+      dispatch(callProcessAction(contextDispatch, true, 0, limit));
+    },
+    refresh: (contextDispatch: (action: any) => any) => {
+      contextDispatch(Actions.historyListRefresh());
+      dispatch(callProcessAction(contextDispatch, true, 0, limit));
+    },
+    loadMore: (
+      contextDispatch: (action: any) => any,
+      paginationQueris: { skip: number; canLoadMore: boolean },
+    ) => {
+      if (paginationQueris.canLoadMore) {
+        contextDispatch(Actions.historyListLoadMore());
+        dispatch(
+          callProcessAction(
+            contextDispatch,
+            false,
+            paginationQueris.skip + limit,
+            limit,
+          ),
+        );
+      }
+    },
+  };
+};
