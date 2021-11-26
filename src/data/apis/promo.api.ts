678fbba9ecc3cce5fbd5280b0f4308ed3e336b06
ryananjasmara <ryananjasmara1997@gmail.com>
2021-11-26 14:28:16 +0700

    update integration potential-promo-product using non-mock

--- src/data/apis/promo.api.ts
+++ src/data/apis/promo.api.ts
@@ -39,14 +39,13 @@ const promoGeneralDetail = (data: models.DetailProcessProps) => {
 };
 /** => potential promo product */
 const potentialPromoProduct = (data: models.DetailProcessProps) => {
-  const mockHost = 'https://690d9a8b-8da9-4142-b577-d543b2682e7f.mock.pstmn.io';
   const path = `potential-promo-product/${data.id}`;
-  return apiMappingMock<models.PotentialPromoProductProps>(
-    mockHost,
+  return apiMapping<models.PotentialPromoProductProps>(
+    'auth',
     path,
     'discount',
     'v1',
-    'LIST',
+    'DETAIL',
   );
 };
 /** => delete reserve discount */
