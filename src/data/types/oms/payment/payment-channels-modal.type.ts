c30cc022dedcb216651f1d9dcc58449b0a4a8eb6
ayu-khairina <ayu@sinbad.co.id>
2021-11-28 19:50:13 +0700

    integrate selected payment type

--- src/data/types/oms/payment/payment-channels-modal.type.ts
+++ src/data/types/oms/payment/payment-channels-modal.type.ts
@@ -2,3 +2,7 @@
 export const MERGE_PAYMENT_CHANNELS = 'MergePaymentChannels';
 /** ==> update selected payment type */
 export const SELECTED_PAYMENT_TYPE = 'SelectedPaymentType';
+/** ==> update selected payment channel */
+export const SELECTED_PAYMENT_CHANNEL = 'SelectedPaymentChannel';
+/** ==> update payment channels */
+export const UPDATE_PAYMENT_CHANNELS = 'UpdatePaymentChannels';
