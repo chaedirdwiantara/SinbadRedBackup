77c0e3edf5623edaa8ee362faed7bac2178f9e19
aliisetia <ali.setia@sinbad.co.id>
2021-11-29 10:58:15 +0700

    Add hook for tag functionality and update brandlist hook action function

--- src/screens/brand/functions/brand-hook.function.ts
+++ src/screens/brand/functions/brand-hook.function.ts
@@ -1,7 +1,8 @@
 /** === IMPORT PACKAGES === */
 import { useDispatch } from 'react-redux';
-/** === IMPORT INTERNAL === */
+/** === IMPORT FUNCTION === */
 import * as Actions from '@actions';
+/** === IMPORT TYPE === */
 import * as models from '@models';
 /** === FUNCTIONS === */
 /** === Fetch Brand Related === */
@@ -10,11 +11,13 @@ const callProcessAction = (
   loading: boolean,
   skip: number,
   limit: number,
+  queryOptions?: models.BrandListQueryOptions,
 ) => {
   return Actions.brandListProcess(contextDispatch, {
     loading,
     skip,
     limit,
+    ...queryOptions,
   });
 };
 
@@ -23,22 +26,39 @@ const useBrandListAction = () => {
   const limit = 10;
 
   return {
-    fetch: (contextDispatch: (action: any) => any) => {
+    fetch: (
+      contextDispatch: (action: any) => any,
+      queryOptions?: models.BrandListQueryOptions,
+    ) => {
       contextDispatch(Actions.brandListReset());
-      dispatch(callProcessAction(contextDispatch, true, 0, limit));
+      dispatch(
+        callProcessAction(contextDispatch, true, 0, limit, queryOptions),
+      );
     },
-    refresh: (contextDispatch: (action: any) => any) => {
+    refresh: (
+      contextDispatch: (action: any) => any,
+      queryOptions?: models.BrandListQueryOptions,
+    ) => {
       contextDispatch(Actions.brandListRefresh());
-      dispatch(callProcessAction(contextDispatch, true, 0, limit));
+      dispatch(
+        callProcessAction(contextDispatch, true, 0, limit, queryOptions),
+      );
     },
     loadMore: (
       contextDispatch: (action: any) => any,
       list: models.ListItemProps<models.BrandListSuccessProps[]>,
+      queryOptions?: models.BrandListQueryOptions,
     ) => {
       if (list.data.length < list.total) {
         contextDispatch(Actions.brandListLoadMore());
         dispatch(
-          callProcessAction(contextDispatch, false, list.skip + limit, limit),
+          callProcessAction(
+            contextDispatch,
+            false,
+            list.skip + limit,
+            limit,
+            queryOptions,
+          ),
         );
       }
     },
