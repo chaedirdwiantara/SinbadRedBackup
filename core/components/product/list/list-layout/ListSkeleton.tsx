8b23354e1adabcb2546ea47624b966392429dd2e
aliisetia <ali.setia@sinbad.co.id>
2021-11-29 10:57:41 +0700

    Change skeleton components to ScrollView

--- core/components/product/list/list-layout/ListSkeleton.tsx
+++ core/components/product/list/list-layout/ListSkeleton.tsx
@@ -1,26 +1,20 @@
 /** === IMPORT PACKAGES ===  */
 import React, { FC } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, ScrollView } from 'react-native';
 /** === IMPORT COMPONENT === */
 import { ListCardSkeleton } from './ListCardSkeleton';
 /** === COMPONENT === */
-export const ListSkeleton: FC = () => {
-  /** === VIEW === */
-  /** => Skeleton Item */
-  const renderSkeletonItem = (props: any) => (
-    <ListCardSkeleton key={props.index} />
-  );
-  /** => Main */
-  return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        contentContainerStyle={{ padding: 16 }}
-        data={Array(8).fill(0)}
-        scrollEnabled={false}
-        showsVerticalScrollIndicator={false}
-        renderItem={renderSkeletonItem}
-        keyExtractor={(_, index) => index.toString()}
-      />
-    </View>
-  );
-};
+export const ListSkeleton: FC = () => (
+  <View style={{ flex: 1 }}>
+    <ScrollView
+      scrollEnabled={false}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ padding: 16 }}>
+      {Array(8)
+        .fill(0)
+        .map((_, index) => (
+          <ListCardSkeleton key={index} />
+        ))}
+    </ScrollView>
+  </View>
+);
