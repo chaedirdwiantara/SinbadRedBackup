6bd3bd159c5b8903bc43f852cefd28c4ddbdb20a
aliisetia <ali.setia@sinbad.co.id>
2021-12-03 16:43:49 +0700

    Change brand list item type name and fix bug brand action not triggering loading

--- src/data/sagas/brand.saga.ts
+++ src/data/sagas/brand.saga.ts
@@ -1,17 +1,17 @@
-/** === IMPORT PACKAGE HERE === */
+/** === IMPORT PACKAGES === */
 import { put, call, takeLatest } from 'redux-saga/effects';
-/** === IMPORT EXTERNAL FUNCTION HERE === */
+/** === IMPORT INTERNAL === */
 import { BrandApi } from '../apis/brand.api';
 import * as ActionCreators from '@actions';
 import * as types from '@types';
 import * as models from '@models';
 /** === FUNCTION === */
-/** => brand list */
+/** => List */
 function* brandList(action: models.ListProcessAction) {
   try {
-    const response: models.ListSuccessProps<models.BrandListSuccessProps> =
+    const response: models.ListSuccessProps<models.BrandListItem[]> =
       yield call(() => {
-        return BrandApi.brandList(action.payload);
+        return BrandApi.getList(action.payload);
       });
     yield action.contextDispatch(ActionCreators.brandListSuccess(response));
     yield put(ActionCreators.brandListSuccess(response));
@@ -20,7 +20,7 @@ function* brandList(action: models.ListProcessAction) {
     yield put(ActionCreators.brandListFailed(error));
   }
 }
-/** === LISTEN FUNCTION === */
+/** === LISTENER === */
 function* BrandSaga() {
   yield takeLatest(types.BRAND_LIST_PROCESS, brandList);
 }
