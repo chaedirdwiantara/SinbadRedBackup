3914ccd2430c8459a88520445e9da6d6a27ec190
bagaspp-sinbad <bagas.prasetya@sinbad.co.id>
2021-12-08 15:17:59 +0700

    wip

---
+++ src/screens/quest/styles/quest-list.style.ts
@@ -0,0 +1,81 @@
+import { StyleSheet } from 'react-native';
+import { color } from 'react-native-sinbad-ui';
+
+const QuestStyles = StyleSheet.create({
+  shadowForBox: {
+    borderWidth: 0,
+    backgroundColor: color.white,
+    shadowColor: color.black100,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 2,
+  },
+  boxMainContent: {
+    marginBottom: 8,
+    marginHorizontal: 16,
+    borderRadius: 8,
+  },
+  cardImage: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 7 / 2,
+    resizeMode: 'cover',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  cardMainContent: {
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    padding: 16,
+    backgroundColor: color.white,
+  },
+  floatingDateContainer: {
+    position: 'absolute',
+    zIndex: 2,
+    right: 10,
+    top: -15,
+  },
+  floatingDate: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: color.yellow50,
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    borderRadius: 100,
+  },
+  cardBottomContent: {
+    flexDirection: 'row',
+    marginTop: 16,
+    justifyContent: 'space-between',
+  },
+  progressBarContainer: {
+    flex: 1,
+  },
+  fullBar: {
+    height: 8,
+    flex: 1,
+    backgroundColor: color.black10,
+    borderRadius: 8,
+  },
+  progressBar: {
+    position: 'absolute',
+    height: '100%',
+    left: 0,
+    borderRadius: 8,
+    backgroundColor: color.red50,
+  },
+  cardButton: {
+    borderRadius: 4,
+    backgroundColor: color.red50,
+    padding: 6,
+    width: 79,
+    alignItems: 'center',
+    marginLeft: 18,
+  },
+});
+
+export default QuestStyles;
---
+++ src/screens/quest/views/quest-list.view.tsx
@@ -0,0 +1,227 @@
+/** === IMPORT PACKAGE HERE === */
+import React, { FC, useState } from 'react';
+import { View, Image, FlatList, TouchableOpacity } from 'react-native';
+import {
+  SnbContainer,
+  SnbTopNav,
+  SnbText,
+  SnbTabs,
+  color,
+  SnbIconHint,
+} from 'react-native-sinbad-ui';
+import moment from 'moment';
+/** === IMPORT EXTERNAL FUNCTION HERE === */
+import { goBack } from '../function';
+import { QuestListStyles } from '../styles';
+
+interface QuestCardProps {
+  id: number;
+  title: string;
+  image: string;
+  endDate: string;
+  currentTask: number;
+  totalTask: number;
+  status: string;
+  onCardPress?: () => void;
+}
+
+const mockData = {
+  data: [
+    {
+      id: 11,
+      title: 'ini quest consent letter + konfirmasi toko',
+      image:
+        'https://sinbad-website-sg.s3.ap-southeast-1.amazonaws.com/staging/quest-images/image_1638262982539.png',
+      endDate: '2022-08-31',
+      currentTask: 3,
+      totalTask: 4,
+      status: 'on_progress',
+    },
+    {
+      id: 12,
+      title: 'ini quest consent letter + konfirmasi toko',
+      image:
+        'https://sinbad-website-sg.s3.ap-southeast-1.amazonaws.com/staging/quest-images/image_1638262982539.png',
+      endDate: '2022-08-31',
+      currentTask: 2,
+      totalTask: 4,
+      status: 'on_progress',
+    },
+    {
+      id: 13,
+      title: 'ini quest consent letter + konfirmasi toko',
+      image:
+        'https://sinbad-website-sg.s3.ap-southeast-1.amazonaws.com/staging/quest-images/image_1638262982539.png',
+      endDate: '2022-08-31',
+      currentTask: 4,
+      totalTask: 4,
+      status: 'on_progress',
+    },
+    {
+      id: 14,
+      title: 'ini quest consent letter + konfirmasi toko',
+      image:
+        'https://sinbad-website-sg.s3.ap-southeast-1.amazonaws.com/staging/quest-images/image_1638262982539.png',
+      endDate: '2022-08-31',
+      currentTask: 1,
+      totalTask: 4,
+      status: 'on_progress',
+    },
+  ],
+  meta: {
+    limit: 1,
+    skip: 0,
+    total: 1,
+  },
+};
+
+/** === CONSTANTS AND DUMMIES === */
+const questTabs = ['Semua', 'Berjalan', 'Selesai'];
+
+/** === COMPONENT === */
+const QuestListView: FC = () => {
+  /** === HOOK === */
+  const [activeTab, setActiveTab] = useState(0);
+  /** === VIEW === */
+  /** => Header */
+  const renderHeader = () => {
+    return <SnbTopNav.Type3 type="red" title="Quest" backAction={goBack} />;
+  };
+  /** => Tabs */
+  const renderTabs = () => (
+    <SnbTabs.Fixed
+      tabs={questTabs}
+      activeTabs={activeTab}
+      onChangeActiveTabs={(tabIndex: number) => setActiveTab(tabIndex)}
+    />
+  );
+  /** => render floating date */
+  const renderFloatingDate = (date: string) => {
+    return (
+      <View style={QuestListStyles.floatingDate}>
+        <SnbIconHint
+          iconName={'schedule'}
+          size={16}
+          iconColor={color.black100}
+          badgeColor={'yellow'}
+        />
+        <View style={{ marginLeft: 8 }}>
+          <SnbText.C2>{moment(date).format('DD MMMM YYYY')}</SnbText.C2>
+        </View>
+      </View>
+    );
+  };
+  /** => render progress bar */
+  const renderProgressBar = (currentTask: number, totalTask: number) => {
+    const progress = currentTask === 0 ? 10 : (currentTask / totalTask) * 100;
+    return (
+      <View>
+        <View style={QuestListStyles.fullBar}>
+          <View
+            style={[
+              QuestListStyles.progressBar,
+              {
+                width: `${progress}%`,
+              },
+            ]}
+          />
+        </View>
+        <View style={{ marginTop: 4 }}>
+          {currentTask === totalTask ? (
+            <SnbText.B3>Silakan klaim voucher Anda</SnbText.B3>
+          ) : (
+            <SnbText.B3>{`${currentTask} dari ${totalTask} tahap selesai`}</SnbText.B3>
+          )}
+        </View>
+      </View>
+    );
+  };
+
+  /** => render button */
+  const renderButton = (currentTask: number, totalTask: number) => {
+    let buttonText;
+
+    if (currentTask === 0) {
+      buttonText = 'Mulai';
+    } else if (currentTask < totalTask) {
+      buttonText = 'Lanjut';
+    } else if (currentTask === totalTask) {
+      buttonText = 'Klaim';
+    }
+
+    if (currentTask === totalTask) {
+      return (
+        <TouchableOpacity
+          style={QuestListStyles.cardButton}
+          onPress={() => null}>
+          <SnbText.B4 color={color.white}>{buttonText}</SnbText.B4>
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <TouchableOpacity
+          style={QuestListStyles.cardButton}
+          onPress={() => null}>
+          <SnbText.B4 color={color.white}>{buttonText}</SnbText.B4>
+        </TouchableOpacity>
+      );
+    }
+  };
+  /** => render item */
+  const renderItem = ({ item }: { item: QuestCardProps }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => null}
+        style={[QuestListStyles.shadowForBox, QuestListStyles.boxMainContent]}>
+        <Image source={{ uri: item.image }} style={QuestListStyles.cardImage} />
+        <View style={[QuestListStyles.cardMainContent]}>
+          <View style={QuestListStyles.floatingDateContainer}>
+            {renderFloatingDate(item.endDate)}
+          </View>
+          <SnbText.H4>{item.title}</SnbText.H4>
+          <View style={QuestListStyles.cardBottomContent}>
+            <View style={QuestListStyles.progressBarContainer}>
+              {renderProgressBar(item.currentTask, item.totalTask)}
+            </View>
+            {renderButton(item.currentTask, item.totalTask)}
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  /** => Quest List */
+  const renderQuestList = () => {
+    return (
+      <View style={{ paddingVertical: 8, flex: 1 }}>
+        <FlatList
+          // contentContainerStyle={NotificationStyle.boxFlatlist}
+          data={mockData.data}
+          renderItem={renderItem}
+          // keyExtractor={(item, index) => index.toString()}
+          // refreshing={notificationListState.refresh}
+          // onRefresh={onHandleRefresh}
+          // onEndReachedThreshold={0.1}
+          // onEndReached={onHandleLoadMore}
+          // ItemSeparatorComponent={renderSeparator}
+          showsVerticalScrollIndicator
+        />
+      </View>
+    );
+  };
+  /** => Content */
+  const renderContent = () => (
+    <>
+      {renderTabs()}
+      {renderQuestList()}
+    </>
+  );
+  /** => Main */
+  return (
+    <SnbContainer color="white">
+      {renderHeader()}
+      {renderContent()}
+    </SnbContainer>
+  );
+};
+
+export default QuestListView;
