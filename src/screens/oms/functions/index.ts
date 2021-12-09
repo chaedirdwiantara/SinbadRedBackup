4596ef7ebe23eed840be10a0ae0d5c13ef339b78
Tatas Fachrul <tatasfachrul@gmail.com>
2021-12-09 15:43:24 +0700

    SR-1105 -> merge all function to index

--- src/screens/oms/functions/index.ts
+++ src/screens/oms/functions/index.ts
@@ -6,6 +6,7 @@ export * from './checkout-success/checkout-success.function';
 export * from './shopping-cart/shopping-cart-hook.function';
 export * from './checkout';
 export * from './verification-order/verification-order-hook.function';
+export * from './checkout/create-orders-hook.function';
 /**
  * ================================================================
  * NOTES
