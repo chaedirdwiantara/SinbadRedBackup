b52b51ce8582141375b2d4011a48256966c4f107
aliisetia <ali.setia@sinbad.co.id>
2021-12-16 14:24:08 +0700

    Integrate with api, connect components to page, but still minus countdown

--- src/screens/history/functions/history-list/history-list.hook.function.ts
+++ src/screens/history/functions/history-list/history-list.hook.function.ts
@@ -1,24 +1,43 @@
-/** === IMPORT PACKAGES === */
+/** === IMPORT PACKAGE === */
 import { useDispatch } from 'react-redux';
 /** === IMPORT INTERNAL === */
 import * as Actions from '@actions';
-
+import * as models from '@models';
 /** === FUNCTIONS === */
+const callProcessAction = (
+  contextDispatch: (action: any) => any,
+  loading: boolean,
+  skip: number,
+  limit: number,
+  queryOptions?: models.HistoryListQueryOptions,
+) => {
+  return Actions.historyListProcess(contextDispatch, {
+    loading,
+    skip,
+    limit,
+    ...queryOptions,
+  });
+};
+
 export const useOrderStatusActions = () => {
   const dispatch = useDispatch();
+
   return {
     fetch: (contextDispatch: (action: any) => any) => {
-      dispatch(Actions.orderStatusProcess(contextDispatch));
-    },
-    reset: (contextDispatch: (action: any) => any) => {
-      contextDispatch(Actions.orderStatusRefresh);
+      dispatch(
+        Actions.orderStatusProcess(contextDispatch, {
+          loading: true,
+          limit: 0,
+          skip: 0,
+        }),
+      );
     },
   };
 };
 
 export const usePaymentStatus = () => {
   const dispatch = useDispatch();
-  const data = { loading: true, limit: 0, skip: 0 };
+
   return {
     list: (contextDispatch: (action: any) => any) => {
       dispatch(
@@ -32,44 +51,43 @@ export const usePaymentStatus = () => {
   };
 };
 
-/** => List */
-const callProcessAction = (
-  contextDispatch: (action: any) => any,
-  loading: boolean,
-  skip: number,
-  limit: number,
-) => {
-  return Actions.historyListProcess(contextDispatch, {
-    loading,
-    skip,
-    limit,
-  });
-};
 export const useHistoryListActions = () => {
   const dispatch = useDispatch();
   const limit = 10;
 
   return {
-    fetch: (contextDispatch: (action: any) => any) => {
+    fetch: (
+      contextDispatch: (action: any) => any,
+      queryOptions?: models.HistoryListQueryOptions,
+    ) => {
       contextDispatch(Actions.historyListReset());
-      dispatch(callProcessAction(contextDispatch, true, 0, limit));
+      dispatch(
+        callProcessAction(contextDispatch, true, 0, limit, queryOptions),
+      );
     },
-    refresh: (contextDispatch: (action: any) => any) => {
+    refresh: (
+      contextDispatch: (action: any) => any,
+      queryOptions?: models.HistoryListQueryOptions,
+    ) => {
       contextDispatch(Actions.historyListRefresh());
-      dispatch(callProcessAction(contextDispatch, true, 0, limit));
+      dispatch(
+        callProcessAction(contextDispatch, true, 0, limit, queryOptions),
+      );
     },
     loadMore: (
       contextDispatch: (action: any) => any,
-      paginationQueris: { skip: number; canLoadMore: boolean },
+      state: models.ListItemProps<Array<models.OrderParcels>>,
+      queryOptions?: models.HistoryListQueryOptions,
     ) => {
-      if (paginationQueris.canLoadMore) {
+      if (state.data.length < state.total) {
         contextDispatch(Actions.historyListLoadMore());
         dispatch(
           callProcessAction(
             contextDispatch,
             false,
-            paginationQueris.skip + limit,
+            state.skip + limit,
             limit,
+            queryOptions,
           ),
         );
       }
