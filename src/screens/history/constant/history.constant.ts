5506e5f317264409c3d9c609c7e6747996516e40
ayu-khairina <ayu@sinbad.co.id>
2021-12-20 16:59:51 +0700

    add history constant

--- src/screens/history/constant/history.constant.ts
+++ src/screens/history/constant/history.constant.ts
@@ -3,12 +3,12 @@ export const PAY_NOW = 1;
 export const PAY_LATER = 2;
 export const PAY_COD = 3;
 
-/** PAYMENT STATUS */
-export const WAITING_FOR_PAYMENT = 'waiting_for_payment';
+/** BILLING STATUS */
+export const PENDING = 'pending'; // waiting_for_payment
 export const OVERDUE = 'overdue';
 export const PAID = 'paid';
-export const CANCEL = 'cancel';
-export const WAITING_FOR_REFUND = 'waiting_for_refund';
+export const CANCEL = 'cancel'; //payment_failed
+export const REFUND_REQUESTED = 'refund_requested'; //waiting_for_refund
 export const REFUNDED = 'refunded';
 
 /** ORDER STATUS */
