c25218cfd1d439a734bfeb807ffd4c9dfe9bc74f
aliisetia <ali.setia@sinbad.co.id>
2021-12-23 17:49:50 +0700

    Add skeletons for history list card and status tag

---
+++ src/screens/history/components/skeletons/HistoryListSkeleton.tsx
@@ -0,0 +1,22 @@
+/** === IMPORT PACKAGES ===  */
+import React, { FC } from 'react';
+import { View, ScrollView } from 'react-native';
+/** === IMPORT COMPONENT === */
+import { HistoryCardSkeleton } from './HistoryCardSkeleton';
+/** === COMPONENT === */
+export const HistoryListSkeleton: FC = () => (
+  <View style={{ flex: 1 }}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingVertical: 12,
+        paddingHorizontal: 8,
+      }}>
+      {Array(8)
+        .fill(0)
+        .map((_, index) => (
+          <HistoryCardSkeleton key={index} />
+        ))}
+    </ScrollView>
+  </View>
+);
