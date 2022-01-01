25f804e68100fba8fe2450122382b3a0de39bb50
hasapu <hari.sakti.putra@gmail.com>
2022-01-01 23:31:03 +0700

    push notif for foreground

--- core/components/PushNotification.tsx
+++ core/components/PushNotification.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import messaging from '@react-native-firebase/messaging';
 import { NavigationAction } from '@navigation';
 import { isEmpty } from 'lodash';
-import { SnbToast } from 'react-native-sinbad-ui';
+import PushNotifications from 'react-native-push-notification';
 /** === INTERFACE === */
 interface RemoteMessage {
   payload: string;
@@ -11,48 +11,31 @@ interface RemoteMessage {
 }
 /** === COMPONENT === */
 const PushNotification = () => {
-  /** === REF === */
-  const notif = React.useRef<any>();
-  /** === STATE === */
-  const [title, setTitle] = React.useState<string | undefined>('');
-  const [body, setBody] = React.useState<string | undefined>('');
+  /** === ACTION FOR FOREGROUND === */
+  PushNotifications.configure({
+    onNotification: function (notification) {
+      if (notification.data.data) {
+        deepLink(notification.data.data);
+      }
+    },
+    popInitialNotification: true,
+  });
   /** === EFFECT === */
   React.useEffect(() => {
     /** === FOR FOREGROUND === */
     const unSubForeground = messaging().onMessage(async (remoteMessage) => {
-      setTitle(remoteMessage.notification?.title);
-      setBody(remoteMessage.notification?.body);
-      notif.current.show();
+      PushNotifications.localNotification({
+        message: remoteMessage.notification?.body!,
+        title: remoteMessage.notification?.title!,
+        largeIcon: '',
+        smallIcon: 'ic_stat_notif',
+        userInfo: remoteMessage,
+      });
     });
     /** === FOR BACKGROUND === */
     const unSubBackground = messaging().setBackgroundMessageHandler(
       async (remoteMessage) => {
-        const remoteMsg = remoteMessage.data;
-        if (!isEmpty(remoteMsg)) {
-          if (remoteMsg?.screen !== undefined) {
-            switch (remoteMsg?.screen) {
-              case 'HomeView':
-              case 'HistoryListView':
-              case 'HelpView':
-              case 'UserView':
-                NavigationAction.goToMenu(
-                  remoteMsg?.screen,
-                  JSON.parse(remoteMsg.payload),
-                );
-                break;
-              default:
-                NavigationAction.navigate(
-                  remoteMsg?.screen,
-                  JSON.parse(remoteMsg.payload),
-                );
-                break;
-            }
-          } else {
-            NavigationAction.resetToHome();
-          }
-        } else {
-          NavigationAction.resetToHome();
-        }
+        deepLink(remoteMessage.data);
       },
     );
     return () => {
@@ -61,15 +44,29 @@ const PushNotification = () => {
     };
   }, []);
 
-  return (
-    <SnbToast
-      ref={notif}
-      position="top"
-      duration={5000}
-      positionValue={20}
-      message={`${title}\n${body}`}
-    />
-  );
+  const deepLink = (data: any) => {
+    if (!isEmpty(data)) {
+      if (data?.screen !== undefined) {
+        switch (data?.screen) {
+          case 'HomeView':
+          case 'HistoryListView':
+          case 'HelpView':
+          case 'UserView':
+            NavigationAction.goToMenu(data?.screen, JSON.parse(data.payload));
+            break;
+          default:
+            NavigationAction.navigate(data?.screen, JSON.parse(data.payload));
+            break;
+        }
+      } else {
+        NavigationAction.resetToHome();
+      }
+    } else {
+      NavigationAction.resetToHome();
+    }
+  };
+
+  return null;
 };
 /** === EXPORT COMPONENT === */
 export default PushNotification;
