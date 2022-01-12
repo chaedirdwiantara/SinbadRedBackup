af1ecf48af8d54bbbfae5a8d8068ab3b980f22a0
bagaspp-sinbad <bagas.prasetya@sinbad.co.id>
2022-01-12 15:04:50 +0700

    layout recordStock

--- src/screens/quest/styles/quest-task-recordStock.style.ts
+++ src/screens/quest/styles/quest-task-recordStock.style.ts
@@ -44,6 +44,84 @@ const QuestStyles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingVertical: 12,
   },
+  boxContentList: {
+    width: '100%',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 50,
+  },
+  boxContentImage: {
+    marginLeft: 8,
+    marginTop: 28,
+    justifyContent: 'center',
+    height: 74,
+    width: 74,
+    alignItems: 'center',
+  },
+  boxContentListItem: {
+    paddingRight: 8,
+    borderRadius: 4,
+  },
+  fullWidthRatioContainRadius5Image: {
+    resizeMode: 'contain',
+    height: undefined,
+    width: '100%',
+    aspectRatio: 1 / 1,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
+  },
+  promoTagContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    zIndex: 1,
+  },
+  promoTagContent: {
+    justifyContent: 'center',
+    height: 26,
+    paddingLeft: 8,
+    borderTopLeftRadius: 4,
+    backgroundColor: color.green50,
+  },
+  boxContentDesc: {
+    paddingLeft: 16,
+    flex: 1,
+  },
+  boxTitleAndSku: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  boxName: {
+    paddingTop: 23,
+    paddingRight: 51,
+    flex: 1,
+    alignItems: 'flex-start',
+  },
+  boxInput: {
+    paddingTop: 22,
+    paddingHorizontal: 16,
+  },
+  boxCalculation: {
+    paddingTop: 25,
+    paddingBottom: 10,
+    paddingHorizontal: 16,
+  },
+  separator: {
+    borderColor: color.black10,
+    borderWidth: 1,
+    borderRadius: 1,
+    borderStyle: 'solid',
+    marginVertical: 9,
+  },
+  textCalculation: {
+    paddingBottom: 9,
+    marginLeft: 17,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
 });
 
 export default QuestStyles;
--- src/screens/quest/views/quest-task-recordStock.tsx
+++ src/screens/quest/views/quest-task-recordStock.tsx
@@ -10,9 +10,13 @@ import {
   SnbCheckbox,
   SnbText,
   color,
+  SnbImageCompressor,
+  SnbTextField,
 } from 'react-native-sinbad-ui';
+import { Svg, Polygon } from 'react-native-svg';
 import LoadingPage from '@core/components/LoadingPage';
 /** === IMPORT FUNCTIONS === */
+import { Images } from 'src/assets';
 import { toCurrency } from '@core/functions/global/currency-format';
 import { goBack, useQuestTaskAction } from '../function';
 import { useQuestContext } from 'src/data/contexts/quest/useQuestContext';
@@ -75,12 +79,139 @@ const QuestTaskRecordStockView: FC = ({ route }: any) => {
       </View>
     );
   };
+  /** => Render Promo Tag */
+  const PromoTag = () => (
+    <View style={QuestTaskRecordStockStyles.promoTagContainer}>
+      <View style={QuestTaskRecordStockStyles.promoTagContent}>
+        <SnbText.C1 color={color.white}>Promo</SnbText.C1>
+      </View>
+      <View>
+        <Svg height="26" width="26" style={{ marginLeft: -0.1 }}>
+          <Polygon points="0,0 0,26 13,13" fill={color.green50} />
+        </Svg>
+      </View>
+    </View>
+  );
+  /** => Render Calculation */
+  const renderCalculation = () => {
+    return (
+      <View style={QuestTaskRecordStockStyles.boxCalculation}>
+        <SnbText.H4>Perhitungan Untuk:</SnbText.H4>
+        <View style={QuestTaskRecordStockStyles.separator} />
+        <View style={{ paddingVertical: 8 }}>
+          <SnbText.B4>Toko Anda</SnbText.B4>
+        </View>
+        <View style={QuestTaskRecordStockStyles.textCalculation}>
+          <SnbText.B3 align={'left'}>Voucher per Pelanggan</SnbText.B3>
+          <SnbText.B3 align={'right'} color={color.green50}>
+            {toCurrency(20000, { withFraction: false })}
+          </SnbText.B3>
+        </View>
+        <View style={{ paddingVertical: 8 }}>
+          <SnbText.B4>Pelanggan Anda</SnbText.B4>
+        </View>
+        <View style={QuestTaskRecordStockStyles.textCalculation}>
+          <SnbText.B3 align={'left'}>Potongan Harga</SnbText.B3>
+          <SnbText.B3 align={'right'} color={color.green50}>
+            {toCurrency(5000, { withFraction: false })}
+          </SnbText.B3>
+        </View>
+      </View>
+    );
+  };
+  /** => Render Item */
+  const renderItem = () => {
+    return (
+      <View style={QuestTaskRecordStockStyles.boxContentList}>
+        <View
+          style={[
+            QuestTaskRecordStockStyles.boxContentListItem,
+            QuestTaskRecordStockStyles.shadowForBox5,
+          ]}>
+          <View style={{ flexDirection: 'row' }}>
+            <PromoTag />
+            <View style={QuestTaskRecordStockStyles.boxContentImage}>
+              <SnbImageCompressor
+                uri={
+                  'https://images.sinbad.co.id/prod/catalogue-images/17671/image_1591176704340.png'
+                }
+                style={
+                  QuestTaskRecordStockStyles.fullWidthRatioContainRadius5Image
+                }
+                defaultSource={Images.opacityPlaceholder}
+              />
+            </View>
+            <View style={QuestTaskRecordStockStyles.boxContentDesc}>
+              <View style={QuestTaskRecordStockStyles.boxTitleAndSku}>
+                <View style={QuestTaskRecordStockStyles.boxName}>
+                  <SnbText.H4>SGM EKSPLOR 3+ MADU 600 GR GA (KP)</SnbText.H4>
+                  <SnbText.B3>Harga Jual yang Disetujui:</SnbText.B3>
+                  <SnbText.B4 color={color.red50}>
+                    {toCurrency(20000, { withFraction: false })}
+                  </SnbText.B4>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={QuestTaskRecordStockStyles.boxInput}>
+            {/* <SnbText.H4>Jumlah Stok:</SnbText.H4> */}
+            <SnbTextField.Text
+              labelText={'Jumlah Stok:'}
+              placeholder={'Qty.'}
+              type={'default'}
+              value={''}
+              keyboardType={'number-pad'}
+              onChangeText={(text) => console.log(text)}
+              clearText={() => console.log('clear')}
+            />
+            <View style={{ paddingTop: 16 }}>
+              <SnbText.H4>Harga Jual Reguler:</SnbText.H4>
+              <SnbText.B3>
+                Harga SKU reguler yang dijual dari toko ke pelanggan (diluar
+                program/promo)
+              </SnbText.B3>
+              <View style={{ paddingTop: 8 }}>
+                <SnbTextField.Text
+                  // labelText={'Harga Jual Reguler:'}
+                  placeholder={'Rp.'}
+                  type={'default'}
+                  value={''}
+                  keyboardType={'number-pad'}
+                  onChangeText={(text) => console.log(text)}
+                  clearText={() => console.log('clear')}
+                  valMsgSuccess={'Berhasil!'}
+                  valMsgError={
+                    'Masukkan harga yang lebih tinggi dari harga jual yang disetujui'
+                  }
+                />
+              </View>
+            </View>
+          </View>
+          {renderCalculation()}
+        </View>
+      </View>
+    );
+  };
+  /** => Render Button */
+  const renderButton = () => {
+    return (
+      <View style={{ height: 75 }}>
+        <SnbButton.Single
+          type="primary"
+          title={'Selanjutnya'}
+          onPress={() => null}
+        />
+      </View>
+    );
+  };
   /** => Render Content */
   const renderContent = () => {
     return (
       <ScrollView>
         {renderInformation()}
         {renderDisclaimer()}
+        {renderItem()}
+        {renderButton()}
       </ScrollView>
     );
   };
