6fd244e51f52e43e24cc270488deae0ea8a2c75b
ryananjasmara <ryananjasmara1997@gmail.com>
2022-02-08 16:21:11 +0700

    added missing store name in shopping-cart-address

--- src/screens/oms/views/shopping-cart/shopping-cart-address.view.tsx
+++ src/screens/oms/views/shopping-cart/shopping-cart-address.view.tsx
@@ -8,6 +8,9 @@ export const ShoppingCartAddress: FC = () => {
     <View style={{ backgroundColor: color.white, marginTop: 4, padding: 12 }}>
       <SnbText.H4>Alamat Pengiriman</SnbText.H4>
       <SnbDivider style={{ marginVertical: 4 }} />
+      <View style={{ marginBottom: 4 }}>
+        <SnbText.B2>Toko Ahmad</SnbText.B2>
+      </View>
       <SnbText.B4 color={color.black60}>
         Jl. Kemang III No. 18, RT.12 / RW.2, Bangka, Kec. Mampang Prpt., Kota
         Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12730
