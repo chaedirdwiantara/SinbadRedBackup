8867c39451632f51745dc94429c1b045a753f788
Rizqullah <eryz.taufan@sinbad.co.id>
2022-02-15 11:54:31 +0700

    Fix wrong models on updateCart

--- src/data/apis/oms/cart.api.ts
+++ src/data/apis/oms/cart.api.ts
@@ -55,7 +55,7 @@ const updateCart = (
   const mockHost = 'https://d3d7848e-6688-43ae-b6e0-f436565227b4.mock.pstmn.io';
   const path = 'carts';
   return apiMappingMockV3<
-    models.CreateSuccessV3Props<models.AddToCartResponse>
+    models.UpdateSuccessV3Props<models.UpdateCartResponse>
   >(mockHost, path, 'discount', 'v1', 'UPDATE', data);
 };
 
