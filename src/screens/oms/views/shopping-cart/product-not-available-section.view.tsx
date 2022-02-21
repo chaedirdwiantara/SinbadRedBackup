c237ee95cd005829e53c01741f326f8b6e6f6d48
Rizqullah <eryz.taufan@sinbad.co.id>
2022-02-21 18:37:15 +0700

    Adjust base styles for shopping cart

--- src/screens/oms/views/shopping-cart/product-not-available-section.view.tsx
+++ src/screens/oms/views/shopping-cart/product-not-available-section.view.tsx
@@ -18,31 +18,20 @@ export const ProductNotAvailableSection: FC = () => {
       style={{
         flexDirection: 'column',
       }}>
-      <View>
-        <View
-          style={{
-            marginTop: 16,
-            marginBottom: 2,
-            paddingHorizontal: 16,
-            paddingVertical: 16,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            backgroundColor: color.white,
-          }}>
-          <SnbText.B4 color={color.black100}>Tidak bisa diproses</SnbText.B4>
-          <View>
-            <TouchableOpacity
-              onPress={() => {}}
-              style={{
-                width: '100%',
-              }}>
-              <SnbText.B4 color={color.blue50}>Hapus</SnbText.B4>
-            </TouchableOpacity>
-          </View>
+      <View style={ShoppingCartStyles.unavailableHeaderContainer}>
+        <SnbText.B4 color={color.black100}>Tidak bisa diproses</SnbText.B4>
+        <View>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              width: '100%',
+            }}>
+            <SnbText.B4 color={color.blue50}>Hapus</SnbText.B4>
+          </TouchableOpacity>
         </View>
-        <View style={{ ...ShoppingCartStyles.cardContainer, marginTop: 0 }}>
-          <ProductUnavailableView />
-        </View>
+      </View>
+      <View style={{ ...ShoppingCartStyles.cardContainer, marginTop: 0 }}>
+        <ProductUnavailableView />
       </View>
     </View>
   );
--- src/screens/oms/views/shopping-cart/product.view.tsx
+++ src/screens/oms/views/shopping-cart/product.view.tsx
@@ -17,105 +17,79 @@ interface ProductViewProps {}
 
 export const ProductView: FC<ProductViewProps> = ({}) => {
   return (
-    <View
-      style={{
-        ...ShoppingCartStyles.horizontalBottomCardSlot,
-      }}
-      key={'product-a'}>
-      <View
-        style={{
-          flexDirection: 'column',
-          width: '100%',
-        }}>
-        <View style={{ flexDirection: 'row' }}>
-          <View
-            style={{
-              marginRight: 20,
-              justifyContent: 'center',
-            }}>
-            <SnbCheckbox status={'unselect'} onPress={() => {}} />
+    <View style={ShoppingCartStyles.horizontalCardContent}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={ShoppingCartStyles.checkboxContainer}>
+          <SnbCheckbox status={'unselect'} onPress={() => {}} />
+        </View>
+        <TouchableOpacity
+          style={{ alignItems: 'center', justifyContent: 'center' }}
+          onPress={() => {}}>
+          <Image
+            source={{
+              uri: 'https://sinbad-website-sg.s3.ap-southeast-1.amazonaws.com/prod/catalogue-images/15731/image_1617790892428.png',
+            }}
+            style={ShoppingCartStyles.productImg}
+          />
+        </TouchableOpacity>
+        <View style={{ justifyContent: 'center' }}>
+          <View style={{ marginBottom: 5 }}>
+            <SnbBadge.Label type="warning" value="Include PPN 10%" />
           </View>
           <TouchableOpacity
-            style={{ alignItems: 'center', justifyContent: 'center' }}
-            onPress={() => {}}>
-            <Image
-              source={{
-                uri: 'https://sinbad-website-sg.s3.ap-southeast-1.amazonaws.com/prod/catalogue-images/15731/image_1617790892428.png',
-              }}
-              style={{ width: 65, height: 65, marginRight: 5 }}
-            />
+            onPress={() => {}}
+            style={{
+              width: '100%',
+            }}>
+            <SnbText.B4 color={color.black80}>
+              SGM Ananda 1 - Varian Omicron
+            </SnbText.B4>
           </TouchableOpacity>
-          <View style={{ justifyContent: 'center' }}>
-            <View style={{ marginBottom: 5 }}>
-              <SnbBadge.Label type="warning" value="Include PPN 10%" />
-            </View>
-            <TouchableOpacity
-              onPress={() => {}}
-              style={{
-                width: '100%',
-              }}>
-              <SnbText.B4 color={color.black80}>
-                SGM Ananda 1 - Varian Omicron
+          <View
+            style={{
+              flexDirection: 'row',
+            }}>
+            <View style={{ marginRight: 5 }}>
+              <SnbText.B4
+                color={color.black60}
+                textDecorationLine="line-through">
+                {toCurrency(25000, { withFraction: false })}
               </SnbText.B4>
-            </TouchableOpacity>
-            <View
-              style={{
-                flexDirection: 'row',
-              }}>
-              <View style={{ marginRight: 5 }}>
-                <SnbText.B4
-                  color={color.black60}
-                  textDecorationLine="line-through">
-                  {toCurrency(25000, { withFraction: false })}
-                </SnbText.B4>
-              </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <SnbText.B4 color={color.black100}>
-                  {toCurrency(15000, { withFraction: false })}
-                </SnbText.B4>
-                <SnbIcon
-                  name="arrow_drop_down_circle"
-                  style={{ color: color.green50, marginLeft: 5 }}
-                  size={16}
-                />
-              </View>
             </View>
-            <View>
-              <SnbText.B4 color={color.red70}>Tersedia 10 Kardus</SnbText.B4>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <SnbText.B4 color={color.black100}>
+                {toCurrency(15000, { withFraction: false })}
+              </SnbText.B4>
+              <SnbIcon
+                name="arrow_drop_down_circle"
+                style={{ color: color.green50, marginLeft: 5 }}
+                size={16}
+              />
             </View>
           </View>
+          <View>
+            <SnbText.B4 color={color.red70}>Tersedia 10 Kardus</SnbText.B4>
+          </View>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            width: '100%',
-            marginTop: 12,
-          }}>
-          <TouchableOpacity onPress={() => {}} style={{ marginRight: 5 }}>
-            <SnbIcon name="delete_outline" color={color.black80} size={32} />
-          </TouchableOpacity>
-          <SnbNumberCounter
-            value={5}
-            maxLength={6}
-            onBlur={() => {}}
-            onFocus={() => {}}
-            onIncrease={() => {}}
-            onDecrease={() => {}}
-            onChange={() => {}}
-            minusDisabled={false}
-            plusDisabled={false}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            width: '100%',
-            marginTop: 12,
-          }}>
-          <SnbText.B4 color={color.black60}>40pcs dalam 1 Kardus</SnbText.B4>
-        </View>
+      </View>
+      <View style={ShoppingCartStyles.actionContainer}>
+        <TouchableOpacity onPress={() => {}} style={{ marginRight: 5 }}>
+          <SnbIcon name="delete_outline" color={color.black80} size={32} />
+        </TouchableOpacity>
+        <SnbNumberCounter
+          value={5}
+          maxLength={6}
+          onBlur={() => {}}
+          onFocus={() => {}}
+          onIncrease={() => {}}
+          onDecrease={() => {}}
+          onChange={() => {}}
+          minusDisabled={false}
+          plusDisabled={false}
+        />
+      </View>
+      <View style={ShoppingCartStyles.actionContainer}>
+        <SnbText.B4 color={color.black60}>40pcs dalam 1 Kardus</SnbText.B4>
       </View>
     </View>
   );
