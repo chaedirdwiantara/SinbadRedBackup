6b860721d059762b5a9c22e256ac362b5261cc7a
--global <muhammad.ali@ainbad.co.id>
2022-03-10 17:18:38 +0700

    Change to vehicle access amount to 2

--- src/screens/auth/functions/global-hooks.functions.ts
+++ src/screens/auth/functions/global-hooks.functions.ts
@@ -8,8 +8,6 @@ import { useNavigation } from '@react-navigation/core';
 const DEFAULT_VEHICLE_ACCESS_AMOUNT = [
   { id: 1, value: 1 },
   { id: 2, value: 2 },
-  { id: 3, value: 3 },
-  { id: 4, value: 4 },
 ];
 
 export const useInputPhone = () => {
