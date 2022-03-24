707b35519f29e7d3989cc8986333660f07f4b06b
Chaedir Dwiantara <chaedir.dwiantara@sinbad.co.id>
2022-03-24 08:52:34 +0700

    TBP-1151 -> add reset paymentmethod order when back to cart

--- src/screens/oms/views/payment-method/payment-method.view.tsx
+++ src/screens/oms/views/payment-method/payment-method.view.tsx
@@ -259,6 +259,7 @@ const OmsPaymentMethod: FC<PaymentMethodInterface> = (props) => {
   const handleBackToCart = () => {
     updateCartAction.reset(dispatchCart);
     checkoutAction.reset(dispatchCheckout);
+    paymentMethodCreateOrder.reset(dispatchPaymentMethod);
     setExpiredSession(false);
     clearTimeout(timeRef.current);
     goToShoppingCart();
