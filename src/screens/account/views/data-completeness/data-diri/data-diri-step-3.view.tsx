3b801dc56d0d7395d044c53a6b249b4927b2efe3
LawfaL <44112412+LawfaL@users.noreply.github.com>
2022-04-18 16:38:03 +0700

    Layout dan integrasi NPWP

--- src/screens/account/views/data-completeness/data-diri/data-diri-step-3.view.tsx
+++ src/screens/account/views/data-completeness/data-diri/data-diri-step-3.view.tsx
@@ -2,27 +2,55 @@ import React, { useState } from 'react';
 import {
   SnbContainer,
   SnbTopNav,
-  SnbUploadPhotoRules,
   SnbButton,
+  SnbUploadPhotoRules,
+  SnbToast,
+  SnbTextField,
 } from 'react-native-sinbad-ui';
+import { View, Image, BackHandler } from 'react-native';
 import { Stepper, ListOfSteps, ModalBack } from '../../shared/index';
-import { View, BackHandler, ScrollView } from 'react-native';
-import { useFocusEffect } from '@react-navigation/core';
+import { useFocusEffect, useIsFocused } from '@react-navigation/core';
+import { DATA_DIRI_STEP_4_VIEW } from '@screen/account/functions/screens_name';
+import { useCamera } from '@screen/account/functions';
+import { useUploadImageAction } from '@core/functions/hook/upload-image';
+import { contexts } from '@contexts';
 import { useEasyRegistration } from '@screen/account/functions';
-import { renderIF, useCamera } from '@screen/auth/functions';
-import * as models from '@models';
-import OCRNPWPResultContent from '@screen/shared/views/components/ocr-npwp-result-content.component';
+import { NavigationAction } from '@navigation';
 
-interface Props {
-  openModalBack: boolean;
-  onCloseModalBack: (value: boolean) => void;
-}
-
-const Content: React.FC<Props> = (props) => {
+const DataDiriStep3View: React.FC = () => {
+  const [openModalStep, setOpenModalStep] = useState(false);
   const [openModalBack, setOpenModalBack] = useState(false);
-  const { openCameraWithOCR } = useCamera();
-  const [value, setValue] = React.useState<models.IOCRResult | null>(null);
+  const { openCamera, capturedImage, resetCamera } = useCamera();
+  const { upload, save } = useUploadImageAction();
+  const { stateGlobal, dispatchGlobal } = React.useContext(
+    contexts.GlobalContext,
+  );
+  const [backHandle, setBackHandle] = useState(false);
+  const {
+    updateCompleteData,
+    updateCompleteDataState,
+    completeDataState,
+    resetUpdateCompleteData,
+    backToDataCompleteness,
+    refetchCompleteData,
+  } = useEasyRegistration();
+  const isFocused = useIsFocused();
 
+  const [npwp, setNpwp] = React.useState(
+    completeDataState?.data?.userData?.taxNo || '',
+  );
+  const [messageErrorNPWP, setMessageErrorNPWP] = React.useState('');
+
+  const [isNPWPValid, setIsNPWPValid] = React.useState(true);
+  React.useEffect(() => {
+    return () => {
+      save(dispatchGlobal, '');
+      resetCamera();
+      resetUpdateCompleteData();
+    };
+  }, []);
+
+  // HANDLE BACK DEVICE
   const handleBackButton = React.useCallback(() => {
     const backHandler = BackHandler.addEventListener(
       'hardwareBackPress',
@@ -36,89 +64,190 @@ const Content: React.FC<Props> = (props) => {
 
   useFocusEffect(handleBackButton);
 
-  function renderUploadPhotoRules() {
-    return (
-      <View style={{ flex: 1 }}>
-        <SnbUploadPhotoRules
-          rulesTitle="Pastikan Foto NPWP Anda Sesuai Ketentuan"
-          imgSrc={require('@image/npwp_image.png')}
-          buttonLabel="Ambil Foto"
-          rules={[
-            'Pastikan anda menggunakan NPWP sendiri',
-            'Foto NPWP tidak silau dan tidak buram',
-            'Pastikan informasi NPWP bisa terbaca dengan jelas',
-            'Hindari tangan menutup NPWP',
-          ]}
-          action={() => openCameraWithOCR('npwp')}
-          type="vertical"
-          resizeMode="contain"
-          listType="number"
-          blurRadius={3}
-        />
-      </View>
-    );
-  }
+  React.useEffect(() => {
+    if (
+      stateGlobal.uploadImage.data !== null &&
+      capturedImage.data?.type === 'npwp'
+    ) {
+      SnbToast.show('Foto Berhasil Diupload', 2500, { positionValue: 40 });
+      resetCamera();
+    }
 
-  function renderOCRResult() {
+    if (stateGlobal.uploadImage.error !== null) {
+      SnbToast.show('Foto Gagal Diupload', 2500, { positionValue: 40 });
+    }
+  }, [stateGlobal.uploadImage, capturedImage.data?.type]);
+
+  //CONFIRM UPLOAD IMAGE
+  const confirm = () => {
+    if (capturedImage?.data?.url && capturedImage.data?.type === 'npwp') {
+      upload(dispatchGlobal, capturedImage.data.url);
+    } else {
+      if (completeDataState?.data?.userData?.taxImageUrl && isFocused) {
+        NavigationAction.navigate(DATA_DIRI_STEP_4_VIEW);
+      } else {
+        openCamera('npwp');
+      }
+    }
+  };
+
+  //BACK AND SAVE
+  const backSave = () => {
+    if (capturedImage?.data?.url && capturedImage.data?.type === 'npwp') {
+      upload(dispatchGlobal, capturedImage.data.url);
+      setBackHandle(true);
+    } else {
+      backToDataCompleteness();
+    }
+  };
+
+  //FOR CHECK IF IMAGE UPLOADED AND HIT API UPDATE COMPLETENESS DATA
+  React.useEffect(() => {
+    if (stateGlobal.uploadImage.data && capturedImage.data?.type === 'npwp') {
+      updateCompleteData({
+        user: { taxImageUrl: stateGlobal.uploadImage?.data?.url },
+      });
+    }
+  }, [stateGlobal.uploadImage.data, capturedImage.data?.type]);
+
+  // FOR SAVE URL IMAGE TO DB USING API UPDATE COMPLETENESS DATA
+  React.useEffect(() => {
+    if (updateCompleteDataState.data !== null && isFocused) {
+      refetchCompleteData();
+      if (backHandle) {
+        backToDataCompleteness();
+        resetUpdateCompleteData();
+        setBackHandle(false);
+        save(dispatchGlobal, '');
+        resetCamera();
+      } else {
+        NavigationAction.navigate(DATA_DIRI_STEP_4_VIEW);
+        resetUpdateCompleteData();
+        save(dispatchGlobal, '');
+        resetCamera();
+      }
+    }
+  }, [updateCompleteDataState, isFocused]);
+
+  const renderUploadPhotoRules = () => {
     return (
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1 }}>
-          <ScrollView>
-            <OCRNPWPResultContent value={setValue} />
-          </ScrollView>
-        </View>
-        <View style={{ height: 72 }}>
-          <SnbButton.Multiple
-            leftType={'secondary'}
-            rightType={'primary'}
-            leftTitle={'Ubah Foto'}
-            rightTitle={'Lanjutkan'}
-            onPressLeft={() => {}}
-            onPressRight={() => {}}
-            rightDisabled={value?.idNumber === '' || value?.nameOnKTP === ''}
-            leftDisabled={false}
-            rightLoading={false}
+      <SnbUploadPhotoRules
+        rulesTitle="Pastikan Foto NPWP Anda Sesuai Ketentuan"
+        imgSrc={require('@image/npwp_image.png')}
+        buttonLabel="Ambil Foto"
+        rules={[
+          'Pastikan anda menggunakan NPWP sendiri',
+          'Foto NPWP tidak silau dan tidak buram',
+          'Pastikan informasi NPWP bisa terbaca dengan jelas',
+          'Hindari tangan menutup NPWP',
+        ]}
+        action={() => openCamera('npwp')}
+        type="vertical"
+        resizeMode="contain"
+        listType="number"
+        blurRadius={3}
+      />
+    );
+  };
+
+  const renderImagePreview = () => {
+    const isImageCaptured = capturedImage?.data?.type === 'npwp';
+    let uri: string | undefined = '';
+    if (isImageCaptured) {
+      uri = capturedImage?.data?.url;
+    } else {
+      uri = completeDataState?.data?.userData?.taxImageUrl;
+    }
+    return (
+      <View style={{ flex: 1, justifyContent: 'space-between'}}>
+        <View style={{ flex: 1, paddingHorizontal: 20, maxHeight: 370}}>
+          <Image
+            resizeMode="contain"
+            source={{ uri }}
+            borderRadius={4}
+            style={{
+              height: undefined,
+              width: undefined,
+              flex: 1,
+              marginBottom: 10,
+            }}
+          />
+          <SnbTextField.Text
+            type={isNPWPValid ? 'default' : 'error'}
+            value={npwp}
+            maxLength={15}
+            helpText={'Abaikan bila sudah sesuai NPWP'}
+            onChangeText={(text) => {
+              text = text.replace(/[^0-9]/g, '');
+              setNpwp(text);
+              setIsNPWPValid(false);
+              setMessageErrorNPWP('');
+              if (text?.length === 15 || text === '' || text === null) {
+                setMessageErrorNPWP('');
+                setIsNPWPValid(true);
+              } else {
+                setMessageErrorNPWP('Pastikan Nomor NPWP 15 Digit');
+                setIsNPWPValid(false);
+              }
+            }}
+            placeholder={'Masukkan Nomor NPWP'}
+            labelText={'Nomor NPWP'}
+            keyboardType={'number-pad'}
+            mandatory
+            valMsgError={messageErrorNPWP}
           />
         </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 1, height: 75 }}>
+            <SnbButton.Single
+              type="secondary"
+              title="Ulangi"
+              onPress={() => openCamera('npwp')}
+              disabled={false}
+            />
+          </View>
+          <View style={{ flex: 1, height: 75 }}>
+            <SnbButton.Single
+              type={'primary'}
+              loading={stateGlobal.uploadImage.loading}
+              title={'Lanjutkan'}
+              disabled={
+                stateGlobal.uploadImage.loading ||
+                updateCompleteDataState.loading ||
+                !isNPWPValid ||
+                !npwp
+              }
+              onPress={() => updateCompleteData({ user: { taxNo: npwp } })}
+            />
+          </View>
+        </View>
       </View>
     );
-  }
+  };
 
-  return (
-    <View style={{ flex: 1 }}>
-      {renderIF(true, renderUploadPhotoRules(), renderOCRResult())}
-      <ModalBack
-        open={openModalBack || props.openModalBack}
-        closeModal={() => {
-          setOpenModalBack(false);
-          props.onCloseModalBack(false);
-        }}
-        confirm={() => {}}
-      />
-    </View>
-  );
-};
-
-const DataDiriStep3View: React.FC = () => {
-  const [openModalStep, setOpenModalStep] = React.useState(false);
-  const [openModalBack, setOpenModalBack] = React.useState(false);
-  const { completeDataState } = useEasyRegistration();
+  const isImageAvailable =
+    completeDataState?.data?.userData?.taxImageUrl !== null ||
+    capturedImage.data?.type === 'npwp';
 
   return (
     <SnbContainer color="white">
-      <SnbTopNav.Type3
-        backAction={() => setOpenModalBack(true)}
-        type="white"
-        title="Foto NPWP"
-      />
-      <Stepper
-        complete={completeDataState?.data?.userProgress?.completed}
-        total={completeDataState?.data?.userProgress?.total}
-        onPress={() => setOpenModalStep(true)}
-      />
-      <Content
-        openModalBack={openModalBack}
-        onCloseModalBack={setOpenModalBack}
+      <View>
+        <SnbTopNav.Type3
+          backAction={() => setOpenModalBack(true)}
+          type="white"
+          title="Foto NPWP"
+        />
+        <Stepper
+          complete={completeDataState?.data?.userProgress?.completed}
+          total={completeDataState?.data?.userProgress?.total}
+          onPress={() => setOpenModalStep(true)}
+        />
+      </View>
+      {isImageAvailable ? renderImagePreview() : renderUploadPhotoRules()}
+      <ModalBack
+        open={openModalBack}
+        closeModal={() => setOpenModalBack(false)}
+        confirm={() => backSave()}
       />
       <ListOfSteps
         open={openModalStep}
