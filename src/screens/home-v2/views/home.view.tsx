1a8647e225c38fb941a42703650323c6b9a4b1ba
--global <muhammad.ali@ainbad.co.id>
2022-05-27 17:14:33 +0700

    Enhancement on home coachmark

--- src/screens/home-v2/views/home.view.tsx
+++ src/screens/home-v2/views/home.view.tsx
@@ -1,5 +1,12 @@
 import React, { FC, useState } from 'react';
-import { View, ScrollView, Dimensions } from 'react-native';
+import {
+  View,
+  ScrollView,
+  Dimensions,
+  LayoutRectangle,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
 import {
   SnbTopNav2,
   colorV2,
@@ -18,13 +25,16 @@ import {
 } from '@screen/account/views/shared';
 import { useDataAuth } from '@core/redux/Data';
 import { renderIF } from '@screen/auth/functions';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 const CopilotView = walkthroughable(View);
 
 const HomeView: FC = ({ start }: any) => {
   const [keyword, setKeyword] = useState('');
   const { meV2 } = useDataAuth();
+  const tabBarHeight = useBottomTabBarHeight();
+  const [vipBadgeLayout, setVipBadgeLayout] = useState<LayoutRectangle>();
 
   const isBadgeVIPAvailable =
     typeof meV2.data?.data?.isDataCompleted === 'boolean' &&
@@ -63,7 +73,10 @@ const HomeView: FC = ({ start }: any) => {
           backgroundColor: colorV2.bgColor.light,
           marginTop: -4,
         }}>
-        {renderIF(isBadgeVIPAvailable, <UpgradeVIPAccountBadge />)}
+        {renderIF(
+          isBadgeVIPAvailable,
+          <UpgradeVIPAccountBadge getLayout={setVipBadgeLayout} />,
+        )}
         <CopilotStep
           text="Cek promo terbaik setiap hari biar belanja makin hemat."
           order={2}
@@ -90,7 +103,12 @@ const HomeView: FC = ({ start }: any) => {
             order={3}
             name="Jadi anggota VIP Sinbad">
             <CopilotView
-              style={{ position: 'absolute', top: 84, height: 56, width }}
+              style={{
+                height: vipBadgeLayout?.height,
+                position: 'absolute',
+                width,
+                top: 60 + (StatusBar?.currentHeight || 0),
+              }}
             />
           </CopilotStep>
           <CopilotStep
@@ -98,13 +116,10 @@ const HomeView: FC = ({ start }: any) => {
             order={4}
             name="Lihat Pesanan">
             <CopilotView
-              style={{
-                height: 54,
-                width: width * 0.25,
-                position: 'absolute',
-                bottom: -56,
-                left: width * 0.25,
-              }}
+              style={[
+                styles.pesananCoachmark,
+                { height: tabBarHeight, top: height - tabBarHeight },
+              ]}
             />
           </CopilotStep>
         </>,
@@ -113,18 +128,23 @@ const HomeView: FC = ({ start }: any) => {
           order={3}
           name="Lihat Pesanan">
           <CopilotView
-            style={{
-              height: 54,
-              width: width * 0.25,
-              position: 'absolute',
-              bottom: -56,
-              left: width * 0.25,
-            }}
+            style={[
+              styles.pesananCoachmark,
+              { height: tabBarHeight, top: height - tabBarHeight },
+            ]}
           />
         </CopilotStep>,
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  pesananCoachmark: {
+    width: width * 0.25,
+    position: 'absolute',
+    left: width * 0.25,
+  },
+});
 
 export default copilot(copilotOptions(4, 'homeCoachmark'))(HomeView);
