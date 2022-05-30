40ba83d87b411288991337d077bcf716a84f255e
--global <muhammad.ali@ainbad.co.id>
2022-05-30 11:24:00 +0700

    Enhancement on Camera

--- src/screens/shared/views/components/index.ts
+++ src/screens/shared/views/components/index.ts
@@ -1,5 +1,6 @@
 import OCRResultContent from './ocr-result-content.component';
 import OTPInput from './otp-input.component';
 import OTPTimer from './otp-timer.component';
+import Camera from './camera.components';
 
-export { OCRResultContent, OTPInput, OTPTimer };
+export { OCRResultContent, OTPInput, OTPTimer, Camera };
