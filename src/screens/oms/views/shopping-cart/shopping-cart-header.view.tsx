7af44ef5cb9a31d16ef65745980e3fdde5d5123f
ryananjasmara <ryananjasmara1997@gmail.com>
2022-05-31 14:33:07 +0700

    migrate v2 cart header

--- src/screens/oms/views/shopping-cart/shopping-cart-header.view.tsx
+++ src/screens/oms/views/shopping-cart/shopping-cart-header.view.tsx
@@ -1,15 +1,15 @@
 /** === IMPORT PACKAGE HERE ===  */
 import React, { FC } from 'react';
-import { SnbTopNav } from 'react-native-sinbad-ui';
+import { SnbTopNav2 } from 'react-native-sinbad-ui';
 /** === COMPONENT ===  */
 interface ShoppingCartHeaderProps {
   goBack: () => void;
 }
 
 export const ShoppingCartHeader: FC<ShoppingCartHeaderProps> = ({ goBack }) => (
-  <SnbTopNav.Type3
-    type="white"
+  <SnbTopNav2.Type3
     title={'Keranjang'}
+    color={'white'}
     backAction={() => goBack()}
   />
 );
