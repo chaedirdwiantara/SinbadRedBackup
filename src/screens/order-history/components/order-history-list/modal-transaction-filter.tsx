55151cf61114a508f062c536d441b4c9791d2a44
sinbad <mahardicka.nur@sinbad.co.id>
2022-06-03 10:49:41 +0700

    update modal actionsheet & radio

--- src/screens/order-history/components/order-history-list/modal-transaction-filter.tsx
+++ src/screens/order-history/components/order-history-list/modal-transaction-filter.tsx
@@ -3,15 +3,18 @@ import React, {
   memo,
   useCallback,
   useImperativeHandle,
+  useMemo,
   useState,
 } from 'react';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import {
-  SnbBottomSheet,
   SnbText,
-  SnbButton,
   color,
-  SnbRadioButton,
+  SnbBottomSheet2,
+  SnbBottomSheetPart,
+  SnbButton2,
+  SnbRadioGroup,
+  SnbRadio,
 } from 'react-native-sinbad-ui';
 
 interface ModalTransactionProps {
@@ -69,32 +72,40 @@ const ModalTransactionFilter = forwardRef<
     [],
   );
 
-  const Content = useCallback(() => {
+  const Content = useMemo(() => {
     return (
       <View style={{ paddingHorizontal: 16 }}>
-        {transactionFilter.map((i) => (
-          <View key={i.id} style={{ marginTop: 14 }}>
-            <TouchableOpacity
-              style={{ flexDirection: 'row' }}
-              onPress={() => setSelect(i.id)}>
-              <View style={{ flex: 1 }}>
-                {i.id === '' ? (
-                  <SnbText.H4>{i.label}</SnbText.H4>
-                ) : (
-                  <SnbText.B1>{i.label}</SnbText.B1>
-                )}
-              </View>
-              <SnbRadioButton
-                status={select === i.id ? 'selected' : 'unselect'}
-                onPress={() => setSelect(i.id)}
-              />
-            </TouchableOpacity>
-            <View style={styles.div} />
-          </View>
-        ))}
-        <View style={{ height: 72 }}>
-          <SnbButton.Single
-            type="primary"
+        <SnbRadioGroup value={select}>
+          {transactionFilter.map((i) => (
+            <View
+              key={i.id}
+              style={{
+                marginTop: 0,
+              }}>
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+
+                  alignItems: 'center',
+                }}
+                onPress={() => setSelect(i.id)}>
+                <View style={{ flex: 1 }}>
+                  {select === i.id ? (
+                    <SnbText.H4>{i.label}</SnbText.H4>
+                  ) : (
+                    <SnbText.B1>{i.label}</SnbText.B1>
+                  )}
+                </View>
+                <SnbRadio value={i.id} style={{ top: 10 }} />
+              </TouchableOpacity>
+              <View style={styles.div} />
+            </View>
+          ))}
+        </SnbRadioGroup>
+        <View style={{ marginVertical: 14 }}>
+          <SnbButton2.Primary
+            size="large"
+            full
             title="Tampilkan"
             onPress={onSubmitFilter}
           />
@@ -104,16 +115,28 @@ const ModalTransactionFilter = forwardRef<
   }, [select]);
 
   return (
-    <View>
-      <SnbBottomSheet
-        content={Content()}
-        open={show}
-        actionIcon="close"
-        closeAction={() => setShow(false)}
-        isSwipeable={true}
-        title="Filter"
-      />
-    </View>
+    <SnbBottomSheet2
+      content={Content}
+      navigation={
+        <SnbBottomSheetPart.Navigation
+          iconRight1Name="x"
+          onRight1Action={() => setShow(false)}
+        />
+      }
+      title={
+        <SnbBottomSheetPart.Title
+          swipeIndicator
+          title="Filter"
+          titleType="center"
+        />
+      }
+      name="filter-order-history"
+      type="content"
+      contentHeight={350}
+      open={show}
+      snap
+      close={() => setShow(false)}
+    />
   );
 });
 
