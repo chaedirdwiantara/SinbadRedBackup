bcd7b5e66a2c716717bddda34e98bcaa2cd93b74
ryananjasmara <ryananjasmara1997@gmail.com>
2022-06-09 15:24:45 +0700

    update image and svg assets

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -14,4 +14,7 @@ export const Images = {
   registrationComplete: require('./registration_complete.png'),
   registrationIncomplete: require('./registration_incomplete.png'),
   reminder: require('./reminder.png'),
+  cartNotFound: require('./cart_not_found.png'),
+  emptySinbad: require('./empty_sinbad.png'),
+  pendingPayment: require('./pending-payment.png'),
 };
