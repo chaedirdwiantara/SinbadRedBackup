0cb9c8a79f55965915b7b475ac46e662efffd728
Hari Sakti Putra <hari.sakti.putra@gmail.com>
2022-06-14 17:06:20 +0700

    update splash screen, delete react native splash screen

--- core/screens/intro/views/intro-splash.view.tsx
+++ core/screens/intro/views/intro-splash.view.tsx
@@ -1,14 +1,5 @@
 /** === IMPORT PACKAGE HERE ===  */
 import React from 'react';
-import { View } from 'react-native';
-import {
-  SnbContainer,
-  SnbProgress,
-  colorV2,
-  spacingV2 as layout,
-} from 'react-native-sinbad-ui';
-import Svg from '@svg';
-import LinearGradient from 'react-native-linear-gradient';
 import SplashScreen from 'react-native-splash-screen';
 /** === IMPORT EXTERNAL FUNCTION === */
 // import { usePageAfterIntro } from '../functions';
@@ -16,8 +7,6 @@ import { useAuthCoreAction } from '@core/functions/auth';
 import { useDataAuth } from '@core/redux/Data';
 import { NavigationAction } from '@navigation';
 import { useOTP } from '@screen/auth/functions';
-/** === IMPORT STYLE HERE === */
-import IntroStyle from '../styles/intro.style';
 /** === COMPONENT === */
 const IntroSplashView: React.FC = () => {
   const { meV2 } = useDataAuth();
@@ -40,38 +29,24 @@ const IntroSplashView: React.FC = () => {
       if (meV2.data?.data?.isBuyerCategoryCompleted) {
         setTimeout(() => {
           NavigationAction.resetToHome();
+          SplashScreen.hide();
         }, 100);
       } else {
         getLocationPermissions();
+        setTimeout(() => {
+          SplashScreen.hide();
+        }, 100);
       }
     } else if ((!meV2.data || meV2.error) && !meV2.loading) {
       setTimeout(() => {
         NavigationAction.resetToIntroSinbad();
+        SplashScreen.hide();
       }, 100);
     }
   }, [meV2]);
   /** === VIEW === */
   /** => main */
-  return (
-    <SnbContainer color="white">
-      <LinearGradient
-        start={{ x: 0, y: 1 }}
-        end={{ x: 1, y: 0 }}
-        colors={['#870100', '#d43238']}
-        style={{ flex: 1 }}>
-        <View style={IntroStyle.sinbadLogo}>
-          <Svg name={'white_sinbad_logo'} size={100} />
-          <View style={{ marginBottom: -layout.spacing.xxl }}>
-            <Svg name={'splash_highlight_text'} size={200} />
-          </View>
-          <Svg name={'sinbad_onboard'} size={240} />
-        </View>
-        <View style={{ marginBottom: layout.spacing.lg }}>
-          <SnbProgress color={colorV2.bgColor.light} size={50} />
-        </View>
-      </LinearGradient>
-    </SnbContainer>
-  );
+  return null;
 };
 
 export default IntroSplashView;
