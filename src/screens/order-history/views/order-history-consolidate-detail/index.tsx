4cc2361a5917c9f0f7bd2649ed1de09618d18adf
Chaedir Dwiantara <chaedirdwiantara@gmail.com>
2022-06-28 12:00:53 +0700

    TBP-2217 -> initial new page n navigation setup

---
+++ src/screens/order-history/views/order-history-consolidate-detail/index.tsx
@@ -0,0 +1,51 @@
+import React, { memo, useEffect } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
+import { SnbContainer, SnbTopNav2 } from '@sinbad/react-native-sinbad-ui';
+import {
+  StatusOrder,
+  InformationInvoice,
+  InformationDelivery,
+  ListProductOrder,
+  InformationPayment,
+  ActionFooter,
+} from '@screen/order-history/components/order-history-detail';
+//function
+import { NavigationAction } from '@core/functions/navigation';
+import { useOrderHistoryContext } from 'src/data/contexts/order-history/useOrderHistoryContext';
+import { useDetailHistoryOrder } from '../../functions/history-detail';
+
+const OrderHistoryConsolidateDetail = () => {
+  const {
+    stateOrderHistory: {
+      detail: { loading },
+    },
+  } = useOrderHistoryContext();
+
+  const { get, clear } = useDetailHistoryOrder();
+  // get detail data history
+  useEffect(() => {
+    get();
+
+    return () => {
+      clear();
+    };
+  }, []);
+  return (
+    <SnbContainer color="white">
+      <SnbTopNav2.Type3
+        title="Consolidate Detail"
+        color="white"
+        backAction={NavigationAction.back}
+      />
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={get} />
+        }></ScrollView>
+      // <ActionFooter />
+    </SnbContainer>
+  );
+};
+
+export const OrderHistoryConsolidateDetailView = memo(
+  OrderHistoryConsolidateDetail,
+);
