5f93de3e4f6f9cfdadfeb887c151aeab1918bebb
Chaedir Dwiantara <chaedirdwiantara@gmail.com>
2022-06-29 17:21:59 +0700

    TBP-2237 -> add detail-consolidate-history.models

--- src/data/models/order-history/index.ts
+++ src/data/models/order-history/index.ts
@@ -2,3 +2,4 @@ export * from './list-history';
 export * from './detail-history.model';
 export * from './detail-tracking-history.model';
 export * from './update-history.model';
+export * from './detail-consolidate-history.model';
