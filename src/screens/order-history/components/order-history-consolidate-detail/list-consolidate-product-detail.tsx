4e47b444a9124af9642889803e48bb8f9fd28b56
Chaedir Dwiantara <chaedirdwiantara@gmail.com>
2022-06-30 07:50:29 +0700

    TBP-2237 -> add navigation setup to detain n lacak

--- src/screens/order-history/components/order-history-consolidate-detail/list-consolidate-product-detail.tsx
+++ src/screens/order-history/components/order-history-consolidate-detail/list-consolidate-product-detail.tsx
@@ -17,16 +17,24 @@ import { toCurrency } from '@core/functions/global/currency-format';
 import { useOrderHistoryContext } from 'src/data/contexts/order-history/useOrderHistoryContext';
 import { Products } from '@model/order-history/detail-history.model';
 import ConfirmationTime from '../confirmation-time';
+import { NavigationAction } from '@core/functions/navigation';
 
 type CardProps = {
   data: Products;
+  id: string;
 };
 
 const Card: FC<CardProps> = (props) => {
-  const { data } = props;
+  const { data, id } = props;
 
   return (
-    <View style={styles.card}>
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => {
+        NavigationAction.navigate('OrderHistoryDetailView', {
+          id: id,
+        });
+      }}>
       <View style={{ margin: 16 }}>
         {/* product */}
         <View>
@@ -74,8 +82,11 @@ const Card: FC<CardProps> = (props) => {
             <SnbButton2.Link
               title="Lacak"
               size="small"
-              // onPress={onCancelOrder}
-              onPress={() => {}}
+              onPress={() =>
+                NavigationAction.navigate('HistoryTrackingView', {
+                  id: id,
+                })
+              }
               full={true}
             />
           </View>
@@ -83,7 +94,7 @@ const Card: FC<CardProps> = (props) => {
             <SnbButton2.Secondary
               title="Diterima"
               size="small"
-              // onPress={onCancelOrder}
+              // onPress={onConFirmOrder}
               onPress={() => {}}
               outline={true}
               full={true}
@@ -105,7 +116,7 @@ const Card: FC<CardProps> = (props) => {
           {/* ) : null} */}
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
@@ -130,16 +141,15 @@ const ConsolidateListOrderDetail = () => {
     );
   }
 
-  console.log(fristProduct, 'FIRST PRODUCT');
-  console.log(data, 'DATA PRODUCT');
+  // console.log(fristProduct, 'FIRST PRODUCT');
 
   return (
     <>
       <View style={styles.main}>
         <Header title="Daftar Pesanan" />
-        {fristProduct ? <Card data={fristProduct} /> : <View />}
+        {fristProduct ? <Card data={fristProduct} id={data?.id} /> : <View />}
         {showMore ? (
-          listProduct.map((i) => <Card key={i.id} data={i} />)
+          listProduct.map((i) => <Card key={i.id} data={i} id={data?.id} />)
         ) : (
           <View />
         )}
