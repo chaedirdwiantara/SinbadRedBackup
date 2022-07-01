8e1c4647293b63d84187f98cb8d7c907cad13b45
mazdha94 <muhammad.ali@sinbad.co.id>
2022-07-01 15:20:31 +0700

    Add modal saleman warning

--- src/screens/auth/views/shared/index.ts
+++ src/screens/auth/views/shared/index.ts
@@ -1,5 +1,6 @@
 import OTPContent from './otp-content.component';
 import ModalOTPMethod from './modal-otp-method.component';
 import ForceRegistrationModal from './force-registration-modal.component';
+import ModalSalesman from './modal-salesman.component';
 
-export { OTPContent, ForceRegistrationModal, ModalOTPMethod };
+export { OTPContent, ForceRegistrationModal, ModalOTPMethod, ModalSalesman };
