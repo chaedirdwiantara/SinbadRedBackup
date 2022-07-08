1869997cdf49c94787249066f4a7dbc7778e81d6
sinbad <mahardicka.nur@sinbad.co.id>
2022-07-08 16:25:59 +0700

    update modal filter range price pdp list

--- core/components/modal-actions/filter/FilterAction.tsx
+++ core/components/modal-actions/filter/FilterAction.tsx
@@ -1,20 +1,22 @@
 /** === IMPORT PACKAGES === */
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  memo,
+} from 'react';
 import { View, Keyboard } from 'react-native';
 import {
   SnbButton2,
   SnbText2,
-  SnbSlider,
-  Section,
   spacingV2,
   SnbChips2,
+  PriceRangeInputSlider,
 } from 'react-native-sinbad-ui';
-/** === IMPORT FUNCTIONS */
-import debounce from 'lodash/debounce';
 /** === IMPORT TYPE */
 import { BottomActionPressHandlerType } from '@core/components/product/list/BottomAction';
-/** === IMPORT STYLE */
-import { toCurrency } from '@core/functions/global/currency-format';
 /** === TYPE === */
 interface FilterActionProps {
   testID: string;
@@ -40,6 +42,10 @@ interface FilterActionProps {
     idSort?: string;
   }) => void;
 }
+
+interface FilterActionRef {
+  reset: () => void;
+}
 /** === CONSTANT === */
 const { spacing } = spacingV2;
 const filterPrice = [
@@ -57,129 +63,118 @@ const filterPrice = [
   },
 ];
 /** === COMPONENTS === */
-const FilterAction: FC<FilterActionProps> = ({
-  handleSliderChange,
-  idSortFilter,
-  testID,
-  maxPrice,
-  minPrice,
-  setMaxPrice,
-  setMinPrice,
-  onButtonPress,
-}) => {
-  const [value, setValue] = useState([0, 0]);
-  const [priceSort, setPriceSort] = useState<typeof filterPrice[0]>();
-  // function
-  // function slider event
-  const onSlider = useCallback(
-    debounce((val: Array<number>) => {
-      setValue(val);
-    }, 10),
-    [],
-  );
-  // function change input range price
-  const onChangeRange = useCallback(([start, end]: Array<number>) => {
-    setMinPrice(start);
-    setMaxPrice(end);
-  }, []);
-  // function submit button
-  const onSubmit = useCallback(() => {
-    const min = value[0] || undefined;
-    const max = value[1] || undefined;
+const FilterAction = forwardRef<FilterActionRef, FilterActionProps>(
+  (
+    {
+      idSortFilter,
+      testID,
+      maxPrice,
+      minPrice,
+      setMaxPrice,
+      setMinPrice,
+      onButtonPress,
+    },
+    ref,
+  ) => {
+    const [value, setValue] = useState<[number, number]>([0, 0]);
+    const [priceSort, setPriceSort] = useState<typeof filterPrice[0]>();
+    // function
+    // function change input range price
+    const onChangeRange = useCallback(([start, end]: Array<number>) => {
+      setMinPrice(start);
+      setMaxPrice(end);
+    }, []);
+    // function submit button
+    const onSubmit = useCallback(() => {
+      const min = value[0] || undefined;
+      const max = value[1] || undefined;
 
-    Keyboard.dismiss();
+      Keyboard.dismiss();
 
-    onButtonPress({
-      type: 'applyFilter',
-      value: {
-        minPrice: min,
-        maxPrice: max,
-        sort: priceSort?.sort ?? undefined,
-        sortBy: priceSort?.sortBy ?? undefined,
-      },
-      show: false,
-      idSort: priceSort?.id,
-    });
+      onButtonPress({
+        type: 'applyFilter',
+        value: {
+          minPrice: min,
+          maxPrice: max,
+          sort: priceSort?.sort ?? undefined,
+          sortBy: priceSort?.sortBy ?? undefined,
+        },
+        show: false,
+        idSort: priceSort?.id,
+      });
 
-    setMinPrice(min);
-    setMaxPrice(max);
-  }, [value[0], value[1], priceSort?.sort, priceSort?.sortBy, priceSort?.id]);
-  // set filter price
-  const onSetSortPrice = useCallback((params: typeof filterPrice[0]) => {
-    setPriceSort((prev) => {
-      if (prev?.id === params.id) {
-        return undefined;
+      setMinPrice(min);
+      setMaxPrice(max);
+    }, [value[0], value[1], priceSort?.sort, priceSort?.sortBy, priceSort?.id]);
+    // set filter price
+    const onSetSortPrice = useCallback((params: typeof filterPrice[0]) => {
+      setPriceSort((prev) => {
+        if (prev?.id === params.id) {
+          return undefined;
+        }
+        return params;
+      });
+    }, []);
+    // Effect
+    // set default value min max range
+    useEffect(() => {
+      setValue([minPrice, maxPrice]);
+    }, [minPrice, maxPrice]);
+    // set default value sort asc|desc
+    useEffect(() => {
+      if (idSortFilter) {
+        const filter = filterPrice.find((i) => i.id === idSortFilter);
+        setPriceSort(filter);
+      } else {
+        setPriceSort(undefined);
       }
-      return params;
-    });
-  }, []);
-  // Effect
-  // set default value min max range
-  useEffect(() => {
-    setValue([minPrice, maxPrice]);
-  }, [minPrice, maxPrice]);
-  // set default value sort asc|desc
-  useEffect(() => {
-    if (idSortFilter) {
-      const filter = filterPrice.find((i) => i.id === idSortFilter);
-      setPriceSort(filter);
-    } else {
-      setPriceSort(undefined);
-    }
-  }, [idSortFilter]);
-  /** === VIEW === */
-  return (
-    <>
-      <View style={{ marginBottom: spacing.sm }}>
-        <SnbText2.Body.Default>Urutkan</SnbText2.Body.Default>
-      </View>
-      <View style={{ flexDirection: 'row', marginBottom: spacing.xl }}>
-        {filterPrice.map((i) => (
-          <View style={{ marginRight: spacing.sm }} key={i.id}>
-            <SnbChips2.Choice
-              onPress={() => onSetSortPrice(i)}
-              testID={i.id + '.' + testID}
-              active={i.id === priceSort?.id}
-              text={i.label}
-            />
-          </View>
-        ))}
-      </View>
-      <View style={{ marginBottom: spacing.sm }}>
-        <SnbText2.Body.Default>Harga</SnbText2.Body.Default>
-      </View>
-      <Section.PriceRange
-        testID={testID}
-        onChange={onChangeRange}
-        values={[value[0], value[1]]}
-        placeholders={['0', '0']}
-      />
-      <View style={{ marginTop: spacing.xl }}>
-        <SnbSlider
+    }, [idSortFilter]);
+    // register ref
+    useImperativeHandle(ref, () => ({
+      reset: () => {
+        setValue([0, 0]);
+        setPriceSort();
+      },
+    }));
+    /** === VIEW === */
+    return (
+      <>
+        <View style={{ marginBottom: spacing.sm }}>
+          <SnbText2.Body.Default>Urutkan</SnbText2.Body.Default>
+        </View>
+        <View style={{ flexDirection: 'row', marginBottom: spacing.xl }}>
+          {filterPrice.map((i) => (
+            <View style={{ marginRight: spacing.sm }} key={i.id}>
+              <SnbChips2.Choice
+                onPress={() => onSetSortPrice(i)}
+                testID={i.id + '.' + testID}
+                active={i.id === priceSort?.id}
+                text={i.label}
+              />
+            </View>
+          ))}
+        </View>
+        <View style={{ marginBottom: spacing.sm }}>
+          <SnbText2.Body.Default>Harga</SnbText2.Body.Default>
+        </View>
+        <PriceRangeInputSlider
           testID={testID}
-          minValue={value[0]}
-          maxValue={value[1]}
-          higherLimit={1000000}
-          lowerLimit={0}
-          onSliderChange={onSlider}
-          label={{
-            start: toCurrency(value[0], { withFraction: false }),
-            end: toCurrency(value[1], { withFraction: false }),
-          }}
+          value={value}
+          onChange={onChangeRange}
         />
-      </View>
-      <View style={{ marginTop: spacing.xl }}>
-        <SnbButton2.Primary
-          testID={'submit-filter.' + testID}
-          full
-          size="medium"
-          title="Terapkan"
-          onPress={onSubmit}
-          disabled={false}
-        />
-      </View>
-    </>
-  );
-};
+        <View style={{ marginTop: spacing.xl }}>
+          <SnbButton2.Primary
+            testID={'submit-filter.' + testID}
+            full
+            size="medium"
+            title="Terapkan"
+            onPress={onSubmit}
+            disabled={false}
+          />
+        </View>
+      </>
+    );
+  },
+);
 
-export default FilterAction;
+export default memo(FilterAction);
