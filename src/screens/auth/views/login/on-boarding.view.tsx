b8bb1da4e9e4fef703bac2dd22e200c98fa216dc
Hari Sakti Putra <hari.sakti.putra@gmail.com>
2022-07-12 10:40:30 +0700

    try to change jenkins when create bundle map for sentry

--- src/screens/auth/views/login/on-boarding.view.tsx
+++ src/screens/auth/views/login/on-boarding.view.tsx
@@ -107,6 +107,10 @@ const OnBoardingView: React.FC<Props> = () => {
     );
   };
 
+  const content = () => {
+    throw new Error('My first Sentry error!');
+  };
+
   const termsNotice = () => {
     return (
       <View style={styles.termsNoticeContainer}>
@@ -114,16 +118,6 @@ const OnBoardingView: React.FC<Props> = () => {
           Dengan daftar atau masuk, Anda menyetujui Syarat & Ketentuan serta
           Kebijakan Privasi kami
         </SnbText2.Paragraph.Small>
-      </View>
-    );
-  };
-
-  const content = () => {
-    return (
-      <View style={styles.container}>
-        <View />
-        {slideOnBoard()}
-        {termsNotice()}
       </View>
     );
   };
