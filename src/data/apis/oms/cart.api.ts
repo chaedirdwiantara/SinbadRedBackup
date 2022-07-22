6a0eb83af468edf3dedb7f1779b596409c2c59d9
Rizqullah <eryz.taufan@sinbad.co.id>
2022-07-22 17:42:00 +0700

    Remove mock on cart api

--- src/data/apis/oms/cart.api.ts
+++ src/data/apis/oms/cart.api.ts
@@ -4,28 +4,27 @@ import apiMappingMock from '@core/services/apiMappingMockV3';
 import apiMapping from '@core/services/apiMappingV3';
 import * as models from '@models';
 
+// const getCart = () => {
+//   const path = 'carts';
+//   const mockHost = 'https://ef97a830-e9ff-4fa7-87c9-feb0eb94ebf7.mock.pstmn.io';
+//   return apiMappingMock<models.GetCartData>(
+//     mockHost,
+//     path,
+//     'cart',
+//     'v2',
+//     'DETAIL',
+//   );
+// };
 const getCart = () => {
   const path = 'carts';
-  const mockHost = 'https://ef97a830-e9ff-4fa7-87c9-feb0eb94ebf7.mock.pstmn.io';
-  return apiMappingMock<models.GetCartData>(
-    mockHost,
+  return apiMapping<models.GetCartData>(
+    'auth',
     path,
-    'cart',
+    'buyer-cart',
     'v2',
     'DETAIL',
   );
 };
-
-// const getCart = () => {
-//   const path = 'carts';
-//   return apiMapping<models.GetCartData>(
-//     'auth',
-//     path,
-//     'buyer-cart',
-//     'v1',
-//     'DETAIL',
-//   );
-// };
 
 const getTotalCart = () => {
   const path = 'carts/total-products';
@@ -38,32 +37,50 @@ const getTotalCart = () => {
   );
 };
 
+// const addToCart = (
+//   data: models.CreateProcessProps<models.AddToCartPayload>,
+// ) => {
+//   const path = 'carts';
+//   const mockHost = 'https://ef97a830-e9ff-4fa7-87c9-feb0eb94ebf7.mock.pstmn.io';
+//   return apiMappingMock<models.AddToCartResponse>(
+//     mockHost,
+//     path,
+//     'cart',
+//     'v2',
+//     'CREATE',
+//     data.data,
+//   );
+// };
+
 const addToCart = (
   data: models.CreateProcessProps<models.AddToCartPayload>,
 ) => {
   const path = 'carts';
-  const mockHost = 'https://ef97a830-e9ff-4fa7-87c9-feb0eb94ebf7.mock.pstmn.io';
-  return apiMappingMock<models.AddToCartResponse>(
-    mockHost,
+  return apiMapping<models.AddToCartResponse>(
+    'auth',
     path,
-    'cart',
+    'buyer-cart',
     'v2',
     'CREATE',
     data.data,
   );
 };
 
-// const addToCart = (
-//   data: models.CreateProcessProps<models.AddToCartPayload>,
+// const updateCart = (
+//   data: models.UpdateProcessProps<models.UpdateCartPayload>,
 // ) => {
-//   const path = 'carts';
-//   return apiMapping<models.AddToCartResponse>(
-//     'auth',
+//   const path = `carts/${data.data.id}`;
+//   const mockHost = 'https://ef97a830-e9ff-4fa7-87c9-feb0eb94ebf7.mock.pstmn.io';
+//   return apiMappingMock<models.UpdateCartResponse>(
+//     mockHost,
 //     path,
-//     'buyer-cart',
-//     'v1',
-//     'CREATE',
-//     data.data,
+//     'cart',
+//     'v2',
+//     'UPDATE',
+//     {
+//       buyerName: data.data.buyerName,
+//       carts: data.data.carts,
+//     },
 //   );
 // };
 
@@ -71,11 +88,10 @@ const updateCart = (
   data: models.UpdateProcessProps<models.UpdateCartPayload>,
 ) => {
   const path = `carts/${data.data.id}`;
-  const mockHost = 'https://ef97a830-e9ff-4fa7-87c9-feb0eb94ebf7.mock.pstmn.io';
-  return apiMappingMock<models.UpdateCartResponse>(
-    mockHost,
+  return apiMapping<models.UpdateCartResponse>(
+    'auth',
     path,
-    'cart',
+    'buyer-cart',
     'v2',
     'UPDATE',
     {
@@ -84,23 +100,6 @@ const updateCart = (
     },
   );
 };
-
-// const updateCart = (
-//   data: models.UpdateProcessProps<models.UpdateCartPayload>,
-// ) => {
-//   const path = `carts/${data.data.id}`;
-//   return apiMapping<models.UpdateCartResponse>(
-//     'auth',
-//     path,
-//     'buyer-cart',
-//     'v1',
-//     'UPDATE',
-//     {
-//       buyerName: data.data.buyerName,
-//       carts: data.data.carts,
-//     },
-//   );
-// };
 
 const removeCartProduct = ({
   data,
