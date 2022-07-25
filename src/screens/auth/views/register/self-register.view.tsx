6f3205ad62833a012addf18596094ab9a5b69fdf
mazdha94 <muhammad.ali@sinbad.co.id>
2022-07-25 14:01:23 +0700

    Migrate button on register page using footer button

--- src/screens/auth/views/register/self-register.view.tsx
+++ src/screens/auth/views/register/self-register.view.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { View, StyleSheet, ScrollView, Image, Keyboard } from 'react-native';
 import {
   SnbContainer,
-  SnbText2,
   SnbTopNav2,
   SnbTextField2,
-  SnbButton2,
   spacingV2 as layout,
   SnbBottomSheet2Ref,
+  FooterButton,
 } from 'react-native-sinbad-ui';
 import { useNavigation } from '@react-navigation/core';
 import { LOGIN_PHONE_VIEW } from '@screen/auth/functions/screens_name';
@@ -95,49 +94,28 @@ const SelfRegisterView: React.FC = () => {
 
   const buttonRegister = () => {
     return (
-      <View>
-        <View style={styles.button}>
-          <SnbButton2.Primary
-            title={'Lanjut'}
-            onPress={() => {
-              Keyboard.dismiss();
-              checkPhoneRegistration({
-                mobilePhone: phone.value,
-                identifierDeviceId: advertisingId,
-              });
-            }}
-            disabled={
-              phone.value === '' ||
-              phone.valMsgError !== '' ||
-              checkPhoneRegistrationState.loading
-            }
-            loading={checkPhoneRegistrationState.loading}
-            size="medium"
-            full
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginBottom: layout.spacing.md,
-          }}>
-          <SnbText2.Paragraph.Default>
-            Sudah punya akun Sinbad?{' '}
-          </SnbText2.Paragraph.Default>
-          <View style={{ marginLeft: -layout.spacing.md }}>
-            <SnbButton2.Link
-              title="Masuk"
-              size="medium"
-              onPress={() => {
-                phone.clearText();
-                navigate(LOGIN_PHONE_VIEW);
-              }}
-            />
-          </View>
-        </View>
-      </View>
+      <FooterButton.Single
+        title='Lanjut'
+        buttonPress={() => {
+          Keyboard.dismiss();
+          checkPhoneRegistration({
+            mobilePhone: phone.value,
+            identifierDeviceId: advertisingId,
+          })
+        }}
+        loading={checkPhoneRegistrationState.loading}
+        disabled={
+          phone.value === '' ||
+          phone.valMsgError !== '' ||
+          checkPhoneRegistrationState.loading
+        }
+        textLink="Masuk"
+        textLinkPress={() => {
+          phone.clearText();
+          navigate(LOGIN_PHONE_VIEW);
+        }}
+        description="Sudah punya akun Sinbad?"
+      />
     );
   };
 
