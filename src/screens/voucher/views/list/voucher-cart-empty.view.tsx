38c2490a25808feefeee48c55284733183cd0e21
Rizqullah <eryz.taufan@sinbad.co.id>
2022-07-25 10:39:08 +0700

    Adjust voucher illustration

--- src/screens/voucher/views/list/voucher-cart-empty.view.tsx
+++ src/screens/voucher/views/list/voucher-cart-empty.view.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { SnbContainer, SnbText2 } from '@sinbad/react-native-sinbad-ui';
-import { Image, View } from 'react-native';
+import { Content, SnbContainer } from '@sinbad/react-native-sinbad-ui';
+import { View } from 'react-native';
 import { VoucherCartListStyles } from '@screen/voucher/styles';
 
 export const VoucherCartEmpty = () => {
   return (
     <SnbContainer color="grey">
       <View style={VoucherCartListStyles.emptyContainer}>
-        <Image
-          source={require('../../../../assets/images/voucher_not_found.png')}
-          style={VoucherCartListStyles.emptyImage}
+        <Content.Illustration
+          image={require('../../../../assets/images/voucher_not_found.png')}
+          imageStyle={VoucherCartListStyles.emptyImage}
+          title="Voucher Tidak Ditemukan"
+          description="Silahkan ulangi pencarian atau pilih voucher dari halaman utama"
         />
-        <View style={VoucherCartListStyles.emptyTitle}>
-          <SnbText2.Headline.Default>
-            Voucher Tidak Ditemukan
-          </SnbText2.Headline.Default>
-        </View>
-        <SnbText2.Paragraph.Default align="center">
-          Silahkan ulangi pencarian atau pilih voucher dari halaman utama
-        </SnbText2.Paragraph.Default>
       </View>
     </SnbContainer>
   );
