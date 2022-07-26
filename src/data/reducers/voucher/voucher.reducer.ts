66818ac4d6fd4365c6ca7a9acb7087e41caf30ac
ryananjasmara <ryananjasmara1997@gmail.com>
2022-07-26 12:43:19 +0700

    remove reducer voucherGeneral

--- src/data/reducers/voucher/voucher.reducer.ts
+++ src/data/reducers/voucher/voucher.reducer.ts
@@ -5,11 +5,6 @@ import {
   VoucherCartInitialProps,
 } from './voucher-cart/voucher-cart.reducer';
 import {
-  voucherGeneralInitialState,
-  voucherGeneralReducer,
-  VoucherGeneralInitialProps,
-} from './voucher-general/voucher-general.reducer';
-import {
   cancelVoucherInitialState,
   cancelVoucherReducer,
   CancelVoucherInitialProps,
@@ -22,24 +17,21 @@ import {
 /** === TYPE HERE === */
 export type VoucherInitialProps = {
   voucherCart: VoucherCartInitialProps;
-  voucherGeneral: VoucherGeneralInitialProps;
   cancelVoucher: CancelVoucherInitialProps;
   checkSinbadVoucher: CheckSinbadVoucherInitialProps;
 };
 /** === INITIAL HERE === */
 export const voucherInitialState = {
   voucherCart: voucherCartInitialState,
-  voucherGeneral: voucherGeneralInitialState,
   cancelVoucher: cancelVoucherInitialState,
   checkSinbadVoucher: checkSinbadVoucherInitialState,
 };
 /** === EXPORT ALL HERE === */
 export const voucherReducer = (
-  { voucherCart, voucherGeneral, cancelVoucher, checkSinbadVoucher }: any,
+  { voucherCart, cancelVoucher, checkSinbadVoucher }: any,
   action: any,
 ) => ({
   voucherCart: voucherCartReducer(voucherCart, action),
-  voucherGeneral: voucherGeneralReducer(voucherGeneral, action),
   cancelVoucher: cancelVoucherReducer(cancelVoucher, action),
   checkSinbadVoucher: checkSinbadVoucherReducer(checkSinbadVoucher, action),
 });
