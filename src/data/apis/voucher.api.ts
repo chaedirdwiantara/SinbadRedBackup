1923660f2befbd19bbc8413a554722d97a410701
Rizqullah <eryz.taufan@sinbad.co.id>
2022-07-28 15:59:50 +0700

    Remove mock to cancel voucher

--- src/data/apis/voucher.api.ts
+++ src/data/apis/voucher.api.ts
@@ -1,6 +1,5 @@
 /** === IMPORT EXTERNAL FUNCTION === */
 import apiMapping from '@core/services/apiMappingV3';
-import apiMappingMock from '@core/services/apiMappingMockV3';
 import * as models from '@models';
 /** === FUNCTION === */
 /** => voucher detail */
@@ -47,20 +46,14 @@ const checkSinbadVoucher = ({
 /** => cancel reserve voucher */
 const cancelVoucher = () => {
   const path = 'sinbad-vouchers/cancel-reserve-voucher';
-  const mockHost = 'https://fefad299-d7fa-4988-9f70-188fda3275e9.mock.pstmn.io';
-  return apiMappingMock<models.DeleteItemV3Props>(
-    mockHost,
+  return apiMapping<models.DeleteItemV3Props>(
+    'auth',
     path,
     'voucher',
     'v1',
     'DELETE',
   );
 };
-
-// const cancelVoucher = () => {
-//   const path = 'sinbad-vouchers/cancel-reserve-voucher';
-//   return apiMapping('auth', path, 'voucher', 'v1', 'DELETE');
-// };
 
 /** === EXPORT FUNCTIONS === */
 export const VoucherApi = {
