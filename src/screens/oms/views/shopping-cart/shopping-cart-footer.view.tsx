0b055204c2371f4238cafa9da60b99de32ac6e79
ryananjasmara <ryananjasmara1997@gmail.com>
2022-07-29 14:27:02 +0700

    dry run item #4

--- src/screens/oms/views/shopping-cart/shopping-cart-footer.view.tsx
+++ src/screens/oms/views/shopping-cart/shopping-cart-footer.view.tsx
@@ -83,6 +83,7 @@ export const ShoppingCartFooter: FC<FooterProps> = ({
   const { selectedVoucher, resetSelectedVoucher } = useVoucherLocalData();
   const { footerData, setFooterData } = useFooterData();
   const refFooterHeight = useRef(0);
+  const [isDeleteVoucher, setDeleteVoucher] = useState(false);
 
   /** === ACTIONS === */
   const postCheckProductAction = usePostCheckProductAction();
@@ -343,7 +344,11 @@ export const ShoppingCartFooter: FC<FooterProps> = ({
             refFooterHeight.current,
           );
         }, 300);
-      } else if (footerData.totalOrder < 100000 && countTotalProduct > 0) {
+      } else if (
+        footerData.totalOrder < 100000 &&
+        countTotalProduct > 0 &&
+        !isDeleteVoucher
+      ) {
         setTimeout(() => {
           handleParentToast(
             'Min. belanja 100rb untuk checkout',
@@ -447,12 +452,21 @@ export const ShoppingCartFooter: FC<FooterProps> = ({
         onCloseVoucher={
           voucherStatus === 'green'
             ? () => {
+                setDeleteVoucher(true);
                 handleResetSelectedVoucher();
+                const carts = reformatCarts();
+                checkSinbadVoucherAction.fetch(
+                  dispatchVoucher,
+                  true,
+                  null,
+                  carts,
+                );
                 setTimeout(() => {
                   handleParentToast(
                     'Voucher Berhasil Dihapus',
                     refFooterHeight.current,
                   );
+                  setDeleteVoucher(false);
                 }, 300);
               }
             : undefined
