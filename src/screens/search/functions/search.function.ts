e66417042187ccd9d6f092fa219b4a2677c6f2e9
sinbad <mahardicka.nur@sinbad.co.id>
2022-08-11 13:37:34 +0700

    [TBP-3691] search result pages moengage

--- src/screens/search/functions/search.function.ts
+++ src/screens/search/functions/search.function.ts
@@ -1,16 +1,25 @@
 /** === IMPORT FUNCTION === */
 import { deviceId } from '@core/functions/global/device-data';
 import { NavigationAction } from '@navigation';
-import { recordSearch } from '@core/report/moengage/action';
+import {
+  recordSearch,
+  recordSearchResultPage,
+} from '@core/report/moengage/action';
 /** === FUNCTIONS === */
 const goBack = () => {
   NavigationAction.back();
 };
 
-const searchEventMoengage = (method: 'Keyword' | 'Historical') => {
+const prevLastScreen = () => {
   const routes = NavigationAction.getRoutes() ?? [];
   const prevScreen = routes[routes.length - 2]?.name ?? '';
   const lastScreen = routes[routes.length - 1]?.name ?? '';
+
+  return { prevScreen, lastScreen };
+};
+// send moengage report from search event
+const searchEventMoengage = (method: 'Keyword' | 'Historical') => {
+  const { prevScreen, lastScreen } = prevLastScreen();
   const payload = {
     screen_from: prevScreen,
     screen_to: lastScreen,
@@ -19,10 +28,27 @@ const searchEventMoengage = (method: 'Keyword' | 'Historical') => {
   };
   recordSearch(payload);
 };
+// send moengage report every product list success get
+const searchResultPageEventMoengage = (
+  keyword: string,
+  isKeywordFound: boolean,
+) => {
+  const { prevScreen, lastScreen } = prevLastScreen();
+
+  const payload = {
+    device_id: deviceId,
+    is_keyword_found: isKeywordFound,
+    keyword,
+    screen_to: lastScreen,
+    screen_from: prevScreen,
+  };
+
+  recordSearchResultPage(payload);
+};
 
 const goToProduct = (keyword: string, method: 'Keyword' | 'Historical') => {
   NavigationAction.navigate('SearchProductView', { keyword });
   searchEventMoengage(method);
 };
 
-export { goBack, goToProduct };
+export { goBack, goToProduct, searchResultPageEventMoengage };
