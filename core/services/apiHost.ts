c32c8e31d28ccc0b0d7fd993ac4059f6c378449f
mazdha94 <muhammad.ali@sinbad.co.id>
2022-09-02 15:16:45 +0700

    Set geolocation localization to indonesia

--- core/services/apiHost.ts
+++ core/services/apiHost.ts
@@ -3,7 +3,7 @@ import Config from 'react-native-config';
 export const apiHost = {
   base: Config.BASE_URL,
   auth: Config.AUTH_URL,
-  map: `https://maps.googleapis.com/maps/api/geocode/json?key=${Config.GOOGLE_MAPS_API_KEY}`,
+  map: `https://maps.googleapis.com/maps/api/geocode/json?region=ID&language=id&key=${Config.GOOGLE_MAPS_API_KEY}`,
 };
 
 export default apiHost;
